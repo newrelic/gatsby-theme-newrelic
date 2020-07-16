9b5ccfe233e313885490e710b15393fc5b7a02d7
Zack Stickles <zstickles@newrelic.com>
2020-07-16 10:54:12 -0700

    fix: moved configuration files

---
+++ packages/gatsby-theme-newrelic/gatsby-browser.js
@@ -0,0 +1,2 @@
+// tell Gatsby to use the wrapPageElement component
+export { default } from './gatsby/wrap-page-element';
--- packages/gatsby-theme-newrelic/gatsby/gatsby-browser.js
+++
@@ -1,2 +0,0 @@
-// tell Gatsby to use the wrapPageElement component
-export { default } from './wrap-page-element';
