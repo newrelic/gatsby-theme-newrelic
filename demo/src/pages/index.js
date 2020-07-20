1cf0dd003eb46a1300830c86f8dca1dd654e2d52
Jerel Miller <jmiller@newrelic.com>
2020-07-20 13:51:25 -0700

    chore(demo): render an icon on the demo page

--- demo/src/pages/index.js
+++ demo/src/pages/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { Button, NewRelicLogo } from '@newrelic/gatsby-theme-newrelic';
+import { Button, Icon, NewRelicLogo } from '@newrelic/gatsby-theme-newrelic';
 
 const IndexPage = () => (
   <div>
     <NewRelicLogo />
+    <div>
+      Check it out on <Icon type={Icon.TYPE.GITHUB} />
+    </div>
     <h1>Hello, World</h1>
     <p>This is a test</p>
     <Button
