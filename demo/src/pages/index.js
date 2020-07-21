ee698960f86a9d9283512b9a103e544de382b13a
Jerel Miller <jmiller@newrelic.com>
2020-07-21 00:41:25 -0700

    chore(demo): use graphql to get layout config

--- demo/src/pages/index.js
+++ demo/src/pages/index.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { graphql } from 'gatsby';
+import { css } from '@emotion/core';
 import {
   Button,
   Icon,
@@ -6,23 +9,46 @@ import {
   NewRelicLogo,
 } from '@newrelic/gatsby-theme-newrelic';
 
-const IndexPage = () => (
-  <div>
+const IndexPage = ({ data }) => (
+  <>
     <GlobalHeader />
-    <NewRelicLogo />
-    <div>
-      Check it out on <Icon name={Icon.TYPE.GITHUB} />
-    </div>
-    <h1>Hello, World</h1>
-    <p>This is a test</p>
-    <Button
-      onClick={() => console.log('IT IS NOT')}
-      variant={Button.VARIANT.PRIMARY}
-      size={Button.SIZE.LARGE}
+    <div
+      css={css`
+        margin: 0 auto;
+        padding: ${data.site.layout.contentPadding};
+        max-width: ${data.site.layout.maxWidth};
+      `}
     >
-      Or is it?
-    </Button>
-  </div>
+      <NewRelicLogo />
+      <div>
+        Check it out on <Icon name={Icon.TYPE.GITHUB} />
+      </div>
+      <h1>Hello, World</h1>
+      <p>This is a test</p>
+      <Button
+        onClick={() => console.log('IT IS NOT')}
+        variant={Button.VARIANT.PRIMARY}
+        size={Button.SIZE.LARGE}
+      >
+        Or is it?
+      </Button>
+    </div>
+  </>
 );
+
+IndexPage.propTypes = {
+  data: PropTypes.object,
+};
+
+export const pageQuery = graphql`
+  query {
+    site {
+      layout {
+        contentPadding
+        maxWidth
+      }
+    }
+  }
+`;
 
 export default IndexPage;
