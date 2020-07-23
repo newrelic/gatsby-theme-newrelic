c8ea9bb56ff8740581fc1ba660bdc646088c6dd6
Jerel Miller <jmiller@newrelic.com>
2020-07-22 18:19:17 -0700

    chore: add code block example to demo page

--- demo/src/pages/index.js
+++ demo/src/pages/index.js
@@ -4,10 +4,30 @@ import { graphql } from 'gatsby';
 import { css } from '@emotion/core';
 import {
   Button,
+  CodeBlock,
   Icon,
   GlobalHeader,
   NewRelicLogo,
 } from '@newrelic/gatsby-theme-newrelic';
+
+const codeSample = `
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Button = ({ children, ...props }) => (
+  <button type="button" className="button" {...props}>{children}</button>
+);
+
+Button.propTypes = {
+  children: PropTypes.node
+};
+
+export default Button;
+`;
+
+const liveCodeSample = `
+<Button variant={Button.VARIANT.PRIMARY} onClick={() => alert('Hello!')}>Hello!</Button>
+`;
 
 const IndexPage = ({ data }) => (
   <>
@@ -25,6 +45,28 @@ const IndexPage = ({ data }) => (
       </div>
       <h1>Hello, World</h1>
       <p>This is a test</p>
+      <h2>A button</h2>
+      <CodeBlock
+        copyable
+        lineNumbers
+        highlightedLines="5-7,11"
+        fileName="src/components/Button.js"
+        language="jsx"
+      >
+        {codeSample}
+      </CodeBlock>
+      <h2>A live editable button w/ preview</h2>
+      <CodeBlock
+        copyable
+        lineNumbers
+        preview
+        live
+        fileName="src/components/Button.js"
+        language="jsx"
+        scope={{ Button }}
+      >
+        {liveCodeSample}
+      </CodeBlock>
       <Button
         onClick={() => console.log('IT IS NOT')}
         variant={Button.VARIANT.PRIMARY}
