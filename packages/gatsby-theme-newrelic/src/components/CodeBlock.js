3ed2ef70d7d3081ddde6d1c21209b6e3cd76be0f
Jerel Miller <jmiller@newrelic.com>
2020-07-22 17:30:26 -0700

    feat: add a CodeBlock component

---
+++ packages/gatsby-theme-newrelic/src/components/CodeBlock.js
@@ -0,0 +1,185 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { css } from '@emotion/core';
+import { LiveError, LivePreview, LiveProvider } from 'react-live';
+import Button from './Button';
+import CodeEditor from './CodeEditor';
+import Icon from './Icon';
+import CodeHighlight from './CodeHighlight';
+import MiddleEllipsis from 'react-middle-ellipsis';
+import useClipboard from '../hooks/useClipboard';
+import useFormattedCode from '../hooks/useFormattedCode';
+
+const defaultComponents = {
+  Preview: LivePreview,
+};
+
+const CodeBlock = ({
+  children,
+  components: componentOverrides = {},
+  copyable,
+  fileName,
+  formatOptions,
+  highlightedLines,
+  language,
+  lineNumbers,
+  live,
+  preview,
+  scope,
+}) => {
+  const components = { ...defaultComponents, ...componentOverrides };
+  const formattedCode = useFormattedCode(children.trim(), formatOptions);
+  const [copied, copy] = useClipboard();
+  const [code, setCode] = useState(formattedCode);
+
+  useEffect(() => {
+    setCode(formattedCode);
+  }, [formattedCode]);
+
+  return (
+    <LiveProvider code={code} scope={scope}>
+      {preview && (
+        <components.Preview
+          css={css`
+            padding: 2rem;
+            background: var(--color-white);
+            border: 1px solid var(--color-neutrals-100);
+            box-shadow: var(--boxshadow);
+            border-top-left-radius: 4px;
+            border-top-right-radius: 4px;
+          `}
+        />
+      )}
+      <div
+        css={css`
+          background: var(--color-nord-0);
+          border-radius: 4px;
+
+          .light-mode & {
+            background: var(--color-nord-6);
+          }
+
+          ${preview &&
+          css`
+            border-top-left-radius: 0;
+            border-top-right-radius: 0;
+          `};
+        `}
+      >
+        <div
+          css={css`
+            max-height: 26em;
+            overflow: auto;
+          `}
+        >
+          {live ? (
+            <CodeEditor
+              value={code}
+              language={language}
+              lineNumbers={lineNumbers}
+              onChange={setCode}
+            />
+          ) : (
+            <CodeHighlight
+              highlightedLines={highlightedLines}
+              language={language}
+              lineNumbers={lineNumbers}
+            >
+              {code}
+            </CodeHighlight>
+          )}
+        </div>
+        {(copyable || fileName) && (
+          <div
+            css={css`
+              color: var(--color-nord-6);
+              display: flex;
+              justify-content: space-between;
+              align-items: center;
+              background: var(--color-nord-1);
+              border-bottom-left-radius: 4px;
+              border-bottom-right-radius: 4px;
+              padding: 0 1rem;
+              font-size: 0.75rem;
+
+              .light-mode & {
+                color: var(--color-nord-0);
+                background: var(--color-nord-4);
+              }
+            `}
+          >
+            <div
+              css={css`
+                font-family: var(--code-font);
+                white-space: nowrap;
+                overflow: hidden;
+                padding-right: 0.5rem;
+              `}
+            >
+              {fileName && (
+                <MiddleEllipsis>
+                  <span title={fileName}>{fileName}</span>
+                </MiddleEllipsis>
+              )}
+            </div>
+            <Button
+              type="button"
+              variant={Button.VARIANT.PLAIN}
+              onClick={() => copy(code)}
+              size={Button.SIZE.SMALL}
+              css={css`
+                white-space: nowrap;
+              `}
+            >
+              <Icon
+                name={Icon.TYPE.COPY}
+                css={css`
+                  margin-right: 0.5rem;
+                `}
+              />
+              {copied ? 'Copied' : 'Copy'}
+            </Button>
+          </div>
+        )}
+      </div>
+      {(live || preview) && (
+        <LiveError
+          css={css`
+            color: white;
+            background: var(--color-red-400);
+            padding: 0.5rem 1rem;
+            font-size: 0.75rem;
+            overflow: auto;
+            margin-top: 0.5rem;
+            border-radius: 2px;
+          `}
+        />
+      )}
+    </LiveProvider>
+  );
+};
+
+CodeBlock.propTypes = {
+  fileName: PropTypes.string,
+  components: PropTypes.shape({
+    Preview: PropTypes.elementType,
+  }),
+  copyable: PropTypes.bool,
+  children: PropTypes.string.isRequired,
+  formatOptions: PropTypes.object,
+  highlightedLines: PropTypes.string,
+  language: PropTypes.string,
+  lineNumbers: PropTypes.bool,
+  live: PropTypes.bool,
+  preview: PropTypes.bool,
+  scope: PropTypes.object,
+};
+
+CodeBlock.defaultProps = {
+  copyable: true,
+  lineNumbers: false,
+  live: false,
+  preview: false,
+};
+
+export default CodeBlock;
---
+++ packages/gatsby-theme-newrelic/src/components/CodeHighlight.js
@@ -0,0 +1,202 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { css } from '@emotion/core';
+import Highlight from 'prism-react-renderer';
+import Prism from 'prismjs';
+import { partition, range } from '../utils/array';
+
+const CodeHighlight = ({
+  className,
+  children,
+  highlightedLines: highlightedLineString,
+  language,
+  lineNumbers,
+  wrap,
+}) => {
+  const highlightedLines = getHighlightedLines(highlightedLineString);
+
+  return (
+    <Highlight Prism={Prism} code={children.trim()} language={language}>
+      {({ tokens, getLineProps, getTokenProps }) => {
+        const lineNumberWidth = String(tokens.length).length;
+
+        return (
+          <pre
+            css={css`
+              ${nordTheme};
+
+              color: var(--color-nord-6);
+              font-family: var(--code-font);
+              font-size: 0.75rem;
+              display: block;
+              overflow: auto;
+              white-space: ${wrap ? 'pre-wrap' : 'pre'};
+              word-spacing: normal;
+              word-break: normal;
+              tab-size: 2;
+              hyphens: none;
+              text-shadow: none;
+              padding: 1rem;
+
+              ${lineNumbers &&
+              css`
+                .token-line {
+                  display: grid;
+                  grid-template-columns: ${lineNumberWidth}ch 1fr;
+                  grid-gap: 1rem;
+                }
+              `};
+
+              .light-mode & {
+                color: var(--color-nord-0);
+                background: var(--color-nord-6);
+              }
+            `}
+            className={className}
+            data-language={language}
+          >
+            <code
+              css={css`
+                display: table;
+                width: 100%;
+                padding: 0;
+                background: none;
+              `}
+            >
+              {tokens.map((line, idx) => (
+                // eslint-disable-next-line react/jsx-key
+                <div
+                  css={
+                    highlightedLines.has(idx + 1) &&
+                    css`
+                      background: var(--color-nord-2);
+
+                      .light-mode & {
+                        background: var(--color-nord-5);
+                      }
+                    `
+                  }
+                  {...getLineProps({ line, key: idx })}
+                >
+                  {lineNumbers && (
+                    <div
+                      css={css`
+                        user-select: none;
+                        color: var(--color-nord-3);
+                        text-align: right;
+                      `}
+                    >
+                      {idx + 1}
+                    </div>
+                  )}
+                  <div>
+                    {line.map((token, key) => (
+                      // eslint-disable-next-line react/jsx-key
+                      <span {...getTokenProps({ token, key })} />
+                    ))}
+                  </div>
+                </div>
+              ))}
+            </code>
+          </pre>
+        );
+      }}
+    </Highlight>
+  );
+};
+
+const nordTheme = css`
+  .namespace {
+    opacity: 0.7;
+  }
+  .token {
+    &.plain:empty {
+      display: inline-block;
+    }
+
+    &.comment,
+    &.prolog,
+    &.doctype,
+    &.cdata {
+      color: var(--color-nord-3);
+    }
+    &.tag,
+    &.class-name {
+      color: var(--color-nord-7);
+    }
+    &.function {
+      color: var(--color-nord-8);
+    }
+    &.punctuation,
+    &.operator,
+    &.keyword,
+    &.property,
+    &.entity,
+    &.atrule,
+    &.attr-value,
+    &.url {
+      color: var(--color-nord-9);
+    }
+    &.regex,
+    &.important,
+    &.variable {
+      color: var(--color-nord-12);
+    }
+    &.selector,
+    &.attr-name,
+    &.string,
+    &.char,
+    &.builtin,
+    &.inserted {
+      color: var(--color-nord-14);
+    }
+    &.property,
+    &.boolean,
+    &.constant,
+    &.symbol,
+    &.deleted,
+    &.number {
+      color: var(--color-nord-15);
+    }
+    &.important,
+    &.bold {
+      font-weight: bold;
+    }
+    &.italic {
+      font-style: italic;
+    }
+    &.entity {
+      cursor: help;
+    }
+  }
+`;
+
+const getHighlightedLines = (highlightedLineString) => {
+  if (!highlightedLineString) {
+    return new Set();
+  }
+
+  const groups = highlightedLineString.split(',').map((str) => str.trim());
+  const [ranges, lines] = partition(groups, (group) => group.includes('-'));
+
+  const lineRanges = ranges
+    .map((range) => range.split('-').map(Number))
+    .reduce((acc, [a, b]) => acc.concat(range(a, b)), []);
+
+  return new Set(lines.map(Number).concat(lineRanges));
+};
+
+CodeHighlight.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.string.isRequired,
+  highlightedLines: PropTypes.string,
+  language: PropTypes.string,
+  lineNumbers: PropTypes.bool,
+  wrap: PropTypes.bool,
+};
+
+CodeHighlight.defaultProps = {
+  wrap: false,
+};
+
+export default CodeHighlight;
