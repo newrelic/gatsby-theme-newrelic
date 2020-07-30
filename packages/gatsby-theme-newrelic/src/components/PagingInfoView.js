27477dc6099edb208b6437b22a257f874d46071c
lbaker <lbaker@newrelic.com>
2020-07-29 20:44:33 -0700

    chore: Add PagingInfo view

---
+++ packages/gatsby-theme-newrelic/src/components/PagingInfoView.js
@@ -0,0 +1,48 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import { appendClassName } from '@elastic/react-search-ui-views/es/view-helpers';
+
+const sites = [
+  'developer',
+  'opensource',
+  'docs',
+  'discuss',
+  'newrelic',
+  'blog',
+];
+
+function PagingInfo({
+  className,
+  end,
+  searchTerm,
+  start,
+  totalResults,
+  ...rest
+}) {
+  return (
+    <div className={appendClassName('sui-paging-info', className)} {...rest}>
+      Showing{' '}
+      <strong>
+        {start} - {end}
+      </strong>{' '}
+      out of <strong>{totalResults}</strong>
+      {searchTerm && (
+        <>
+          {' '}
+          for <em>{searchTerm}</em> across {sites.length} sites
+        </>
+      )}
+    </div>
+  );
+}
+
+PagingInfo.propTypes = {
+  end: PropTypes.number.isRequired,
+  searchTerm: PropTypes.string.isRequired,
+  start: PropTypes.number.isRequired,
+  totalResults: PropTypes.number.isRequired,
+  className: PropTypes.string,
+};
+
+export default PagingInfo;
