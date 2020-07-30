944bd80cfafc51dea73c31fd974595df38461235
Zack Stickles <zstickles@gmail.com>
2020-07-30 14:48:36 -0700

    chore: only allow buttons to be used once

--- packages/gatsby-theme-newrelic/src/components/Feedback.js
+++ packages/gatsby-theme-newrelic/src/components/Feedback.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { css } from '@emotion/core';
 import Button from './Button';
@@ -13,34 +13,52 @@ const iconStyles = css`
   margin-right: 0.5rem;
 `;
 
-const Feedback = ({ align, onPositiveClick, onNegativeClick, message }) => (
-  <div
-    css={css`
-      text-align: ${align};
-    `}
-  >
-    <p>{message}</p>
+const Feedback = ({ align, onPositiveClick, onNegativeClick, message }) => {
+  const [submitted, updateSubmitted] = useState(false);
+
+  return (
     <div
       css={css`
-        display: flex;
-        justify-content: ${align};
-
-        button {
-          margin-right: 0.5rem;
-        }
+        text-align: ${align};
       `}
     >
-      <Button onClick={onPositiveClick} variant={Button.VARIANT.PRIMARY}>
-        <Icon css={iconStyles} name={Icon.TYPE.THUMBSUP} />
-        Yes
-      </Button>
-      <Button onClick={onNegativeClick} variant={Button.VARIANT.PRIMARY}>
-        <Icon css={iconStyles} name={Icon.TYPE.THUMBSDOWN} />
-        No
-      </Button>
+      <p>{message}</p>
+      <div
+        css={css`
+          display: flex;
+          justify-content: ${align};
+
+          button {
+            margin-right: 0.5rem;
+          }
+        `}
+      >
+        <Button
+          onClick={() => {
+            updateSubmitted(true);
+            onPositiveClick();
+          }}
+          variant={Button.VARIANT.PRIMARY}
+          disabled={submitted}
+        >
+          <Icon css={iconStyles} name={Icon.TYPE.THUMBSUP} />
+          Yes
+        </Button>
+        <Button
+          onClick={() => {
+            updateSubmitted(true);
+            onNegativeClick();
+          }}
+          variant={Button.VARIANT.PRIMARY}
+          disabled={submitted}
+        >
+          <Icon css={iconStyles} name={Icon.TYPE.THUMBSDOWN} />
+          No
+        </Button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 Feedback.ALIGNMENT = ALIGNMENT;
 
