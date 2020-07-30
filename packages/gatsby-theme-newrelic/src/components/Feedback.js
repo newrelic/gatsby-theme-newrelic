e94cf5b9d280736069ede034adf7038006796fef
Zack Stickles <zstickles@gmail.com>
2020-07-30 14:05:56 -0700

    chore: added styles

--- packages/gatsby-theme-newrelic/src/components/Feedback.js
+++ packages/gatsby-theme-newrelic/src/components/Feedback.js
@@ -1,26 +1,52 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { css } from '@emotion/core';
+import Button from './Button';
 
-const Feedback = ({ onPositiveClick, onNegativeClick, message }) => (
-  <div>
-    <h2>{message}</h2>
-    <button type="button" onClick={onPositiveClick}>
-      ya
-    </button>
-    <button type="button" onClick={onNegativeClick}>
-      na
-    </button>
+const ALIGNMENT = {
+  LEFT: 'left',
+  CENTER: 'center',
+};
+
+const Feedback = ({ align, onPositiveClick, onNegativeClick, message }) => (
+  <div
+    css={css`
+      text-align: ${align};
+    `}
+  >
+    <p>{message}</p>
+    <div
+      css={css`
+        display: flex;
+        justify-content: ${align};
+
+        button {
+          margin-right: 0.5rem;
+        }
+      `}
+    >
+      <Button onClick={onPositiveClick} variant={Button.VARIANT.PRIMARY}>
+        Yes
+      </Button>
+      <Button onClick={onNegativeClick} variant={Button.VARIANT.PRIMARY}>
+        No
+      </Button>
+    </div>
   </div>
 );
 
+Feedback.ALIGNMENT = ALIGNMENT;
+
 Feedback.propTypes = {
+  align: PropTypes.oneOf(Object.values(Feedback.ALIGNMENT)),
   onPositiveClick: PropTypes.func.isRequired,
   onNegativeClick: PropTypes.func.isRequired,
   message: PropTypes.string,
 };
 
 Feedback.defaultProps = {
-  message: 'Was this helpful?',
+  align: Feedback.ALIGNMENT.CENTER,
+  message: 'Was this page helpful?',
 };
 
 export default Feedback;
