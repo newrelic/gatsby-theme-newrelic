b134baf8e9510da98af0cbed39ba0b7fc27dccb4
lbaker <lbaker@newrelic.com>
2020-07-31 16:38:56 -0700

    chore: Remove SwiftTypeSearch from theme exports

--- packages/gatsby-theme-newrelic/index.js
+++ packages/gatsby-theme-newrelic/index.js
@@ -10,7 +10,6 @@ export { default as Overlay } from './src/components/Overlay';
 export { default as Portal } from './src/components/Portal';
 export { default as SearchInput } from './src/components/SearchInput';
 export { default as Surface } from './src/components/Surface';
-export { default as SwiftSearch } from './src/components/SwiftSearch';
 export { default as Video } from './src/components/Video';
 
 export { default as formatCode } from './src/utils/formatCode';
