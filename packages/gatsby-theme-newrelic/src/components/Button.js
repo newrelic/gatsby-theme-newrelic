8ff26bb637f24880939fbd1000df7a5ee5f05aab
Jerel Miller <jerelmiller@gmail.com>
2020-08-14 15:23:51 -0700

    chore: slightly more spacing on xs button

--- packages/gatsby-theme-newrelic/src/components/Button.js
+++ packages/gatsby-theme-newrelic/src/components/Button.js
@@ -20,8 +20,8 @@ const styles = {
       font-size: 0.75rem;
     `,
     [SIZES.EXTRA_SMALL]: css`
-      font-size: 0.75rem;
-      padding: 0.25rem 0.625rem;
+      font-size: 0.625rem;
+      padding: 0.375rem 0.625rem;
       border-radius: 0.125rem;
     `,
   },
