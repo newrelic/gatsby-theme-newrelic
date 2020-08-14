d314ff06d1789d795db0eb8609576980e8deffaa
Jerel Miller <jerelmiller@gmail.com>
2020-08-14 15:41:39 -0700

    chore: ensure buttons do not wrap text

--- packages/gatsby-theme-newrelic/src/components/Button.js
+++ packages/gatsby-theme-newrelic/src/components/Button.js
@@ -86,6 +86,7 @@ const Button = styled.button`
   border-width: 1px;
   border-style: solid;
   transition: all 0.09s cubic-bezier(0.25, 0.46, 0.45, 0.94);
+  white-space: nowrap;
 
   &:hover {
     transform: translate3d(0, -1px, 0);
