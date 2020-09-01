775e3370e58320969ff74594f6f45a1ccd0d8bab
Jerel Miller <jerelmiller@gmail.com>
2020-09-01 15:35:09 -0700

    chore: allow environment-specific splitio configuration

--- packages/gatsby-theme-newrelic/gatsby/wrap-root-element.js
+++ packages/gatsby-theme-newrelic/gatsby/wrap-root-element.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import SplitIOProvider from '../src/components/SplitIOProvider';
+import { merge, omit } from 'lodash';
 
-const wrapRootElement = ({ element }, pluginOptions) => {
+const DEFAULT_ENV = 'development';
+
+const getSplitConfig = (pluginOptions) => {
   const { splitio } = pluginOptions;
 
-  return splitio ? (
-    <SplitIOProvider config={splitio}>{element}</SplitIOProvider>
+  const {
+    env = {},
+    resolveEnv = () => process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV,
+  } = splitio;
+
+  const envOptions = env[resolveEnv()] || env[DEFAULT_ENV] || {};
+
+  return merge(omit(splitio, ['env', 'resolveEnv']), envOptions);
+};
+
+const wrapRootElement = ({ element }, pluginOptions) => {
+  return pluginOptions.splitio ? (
+    <SplitIOProvider config={getSplitConfig(pluginOptions)}>
+      {element}
+    </SplitIOProvider>
   ) : (
     element
   );
