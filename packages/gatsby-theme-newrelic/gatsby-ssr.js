a770d0d818f9c1887af76bf323ea078d04ace9a9
Jerel Miller <jerelmiller@gmail.com>
2020-09-01 18:02:51 -0700

    chore: dont include wrapRootElement in gatsby-ssr

--- packages/gatsby-theme-newrelic/gatsby-ssr.js
+++ packages/gatsby-theme-newrelic/gatsby-ssr.js
@@ -1,5 +1,4 @@
 import wrapPageElement from './gatsby/wrap-page-element';
-import wrapRootElement from './gatsby/wrap-root-element';
 import onPreRenderHTML from './gatsby/on-pre-render-html';
 
-export { onPreRenderHTML, wrapPageElement, wrapRootElement };
+export { onPreRenderHTML, wrapPageElement };
