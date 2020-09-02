865e51d883760d60069e910df137c6ee4f3ea254
Jerel Miller <jerelmiller@gmail.com>
2020-09-01 21:17:56 -0700

    chore(demo): set localhost mode if splitio auth key is not defined

--- demo/gatsby-config.js
+++ demo/gatsby-config.js
@@ -22,8 +22,13 @@ module.exports = {
           },
         },
         splitio: {
+          // Mocked features only used when in localhost mode
+          // https://help.split.io/hc/en-us/articles/360020448791-JavaScript-SDK#localhost-mode
+          features: {
+            'developer-website_global-header-gh-buttons': 'on',
+          },
           core: {
-            authorizationKey: process.env.SPLITIO_AUTH_KEY,
+            authorizationKey: process.env.SPLITIO_AUTH_KEY || 'localhost',
           },
         },
       },
