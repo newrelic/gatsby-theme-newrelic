b7f52c8412899f515a8dc2b3d103a520d01a51a9
Jerel Miller <jerelmiller@gmail.com>
2020-09-03 21:02:49 -0700

    chore: import jest-dom matchers

--- setup-test-env.js
+++ setup-test-env.js
@@ -1,6 +1,7 @@
 /* global expect */
 import { createSerializer, matchers } from 'jest-emotion';
 import * as emotion from '@emotion/core';
+import '@testing-library/jest-dom';
 
 expect.extend(matchers);
 expect.addSnapshotSerializer(createSerializer(emotion));
