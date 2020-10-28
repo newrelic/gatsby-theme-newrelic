50f6df1c48f4a3c6de2d91ccf11e7a1a219b20f3
Zack Stickles <zstickles@gmail.com>
2020-10-27 17:12:48 -0700

    chore: updated the styles for the footer

--- packages/gatsby-theme-newrelic/src/components/GlobalFooter.js
+++ packages/gatsby-theme-newrelic/src/components/GlobalFooter.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Button from './Button';
 import Icon from './Icon';
 import Logo from './Logo';
 import ExternalLink from './ExternalLink';
@@ -28,19 +29,13 @@ const Footer = ({ fileRelativePath }) => {
     <footer
       data-swiftype-index={false}
       css={css`
-        align-items: center;
-        border-top: 1px solid var(--divider-color);
         color: var(--secondary-text-color);
-        display: flex;
-        font-size: 0.75rem;
+        background-color: var(--color-neutrals-050);
         grid-area: footer;
-        justify-content: space-between;
-        margin-right: ${layout.contentPadding};
-        padding: ${layout.contentPadding} 0;
         z-index: 1;
 
-        @media (max-width: 1080px) {
-          flex-direction: column;
+        .dark-mode & {
+          background-color: var(--color-dark-050);
         }
 
         a {
@@ -50,75 +45,82 @@ const Footer = ({ fileRelativePath }) => {
     >
       <div
         css={css`
-          display: flex;
+          font-size: 0.75rem;
           align-items: center;
-
-          @media (max-width: 760px) {
-            display: block;
-          }
+          justify-content: space-between;
+          display: flex;
+          padding: ${layout.contentPadding};
         `}
       >
         <Link to="/">
           <Logo />
         </Link>
-        <div
-          css={css`
-            margin-left: 0.5rem;
-            padding-left: 0.5rem;
-            border-left: 1px dotted var(--border-color);
+        <div>
+          {fileRelativePath && (
+            <Button
+              as={ExternalLink}
+              href={`${repository}/blob/${branch}/${fileRelativePath}`}
+              variant={Button.VARIANT.OUTLINE}
+              size={Button.SIZE.SMALL}
+              css={css`
+                margin-right: 1rem;
+              `}
+            >
+              <Icon
+                name="edit"
+                size="1rem"
+                css={css`
+                  margin-right: 0.5rem;
+                `}
+              />
+              Edit this page
+            </Button>
+          )}
 
-            @media (max-width: 760px) {
-              border-left: none;
-              margin-left: 0;
-              padding: 0.5rem;
-            }
-          `}
-        >
-          Copyright &copy; 2020 New Relic Inc.
+          <Button
+            as={ExternalLink}
+            href={`${repository}/issues/new/choose`}
+            variant={Button.VARIANT.OUTLINE}
+            size={Button.SIZE.SMALL}
+          >
+            <Icon
+              name="github"
+              size="1rem"
+              css={css`
+                margin-right: 0.5rem;
+              `}
+            />
+            Create an issue
+          </Button>
         </div>
       </div>
 
       <div
         css={css`
+          background-color: rgba(0, 0, 0, 0.05);
+          font-size: 0.75rem;
+          align-items: center;
+          justify-content: space-between;
           display: flex;
+          padding: 0.5rem ${layout.contentPadding};
 
-          a {
-            padding-right: 0;
-            display: flex;
-            align-items: center;
-
-            &:hover {
-              color: var(--secondary-text-hover-color);
-            }
-
-            &:not(:first-child) {
-              margin-left: 1rem;
-            }
-
-            svg {
-              margin-right: 0.5rem;
-            }
+          .dark-mode & {
+            background-color: rgba(0, 0, 0, 0.2);
           }
         `}
       >
-        <Link to="/terms">
-          <Icon name="pen" size="1rem" />
-          Terms of service
-        </Link>
-
-        {fileRelativePath && (
-          <ExternalLink
-            href={`${repository}/blob/${branch}/${fileRelativePath}`}
-          >
-            <Icon name="edit" size="1rem" />
-            Edit this page
-          </ExternalLink>
-        )}
-
-        <ExternalLink href={`${repository}/issues/new/choose`}>
-          <Icon name="github" size="1rem" />
-          Create an issue
-        </ExternalLink>
+        <div
+          css={css`
+            text-transform: uppercase;
+            font-size: 0.5rem;
+            letter-spacing: 0.1rem;
+          `}
+        >
+          Copyright &copy; 2020 New Relic Inc.
+        </div>
+        <div>
+          <Link to="/terms">Terms of service</Link>
+        </div>
       </div>
     </footer>
   );
--- packages/gatsby-theme-newrelic/src/components/Logo.js
+++ packages/gatsby-theme-newrelic/src/components/Logo.js
@@ -4,7 +4,7 @@ import { css } from '@emotion/core';
 const Logo = () => (
   <div
     css={css`
-      font-size: 1.4rem;
+      font-size: 1.6rem;
 
       &:hover {
         color: var(--secondary-text-hover-color);
