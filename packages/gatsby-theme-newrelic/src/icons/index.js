b6af9545dba6a25c78f90268b763880381179b15
Jerel Miller <jerelmiller@gmail.com>
2020-11-04 23:41:35 -0800

    chore: wrap existing feather icons in FeatherSVG

--- packages/gatsby-theme-newrelic/src/icons/index.js
+++ packages/gatsby-theme-newrelic/src/icons/index.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import FeatherSVG from '../components/FeatherSVG';
+
 import feather from './feather';
 import newrelic from './newrelic';
 
@@ -6,7 +9,21 @@ const withPrefix = (prefix, icons) =>
     Object.entries(icons).map(([name, icon]) => [`${prefix}-${name}`, icon])
   );
 
+// Eventually the feather icons should be a fully wrapped component instead of
+// just the guts of the SVG. These helper functions aim to provide compatibility
+// with the changes to the `Icon` component, while allowing the old
+// implementation to be used simultaneously. These new changes allow the Icon
+// component to use multiple icon sources instead of just feather icons.
+const wrapFeatherIcon = (icon) => (props) => (
+  <FeatherSVG {...props}>{icon}</FeatherSVG>
+);
+
+const wrapFeatherIcons = (icons) =>
+  Object.fromEntries(
+    Object.entries(icons).map(([name, icon]) => [name, wrapFeatherIcon(icon)])
+  );
+
 export default {
-  ...withPrefix('fe', feather),
+  ...withPrefix('fe', wrapFeatherIcons(feather)),
   ...withPrefix('nr', newrelic),
 };
