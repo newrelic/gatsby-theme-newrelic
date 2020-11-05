b998aa0c82ec0396706ae8890c7b9fceff5cf908
Jerel Miller <jerelmiller@gmail.com>
2020-11-04 16:23:45 -0800

    fix(PageTools): remove layout-specific styles

--- packages/gatsby-theme-newrelic/src/components/PageTools/PageTools.js
+++ packages/gatsby-theme-newrelic/src/components/PageTools/PageTools.js
@@ -1,37 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { graphql, useStaticQuery } from 'gatsby';
 import { css } from '@emotion/core';
 import Section from './Section';
 import Title from './Title';
 
 const PageTools = ({ className, children }) => {
-  const {
-    site: { layout },
-  } = useStaticQuery(graphql`
-    query {
-      site {
-        layout {
-          contentPadding
-        }
-      }
-    }
-  `);
-
   return (
     <aside
       data-swiftype-index={false}
       className={className}
       css={css`
-        align-self: start;
-        position: sticky;
-        top: calc(var(--global-header-height) + ${layout.contentPadding});
         border: 1px solid var(--divider-color);
         border-radius: 0.25rem;
-        overflow-y: auto;
-        max-height: calc(
-          100vh - (var(--global-header-height) + ${layout.contentPadding} * 2)
-        );
       `}
     >
       {children}
