b41db0551fc5412d68e63a593550b0fe84ed0850
Zack Stickles <zstickles@gmail.com>
2020-12-23 12:52:29 -0800

    chore: added a default issue description

--- packages/gatsby-theme-newrelic/src/utils/createIssueURL.js
+++ packages/gatsby-theme-newrelic/src/utils/createIssueURL.js
@@ -1,5 +1,33 @@
 const DEFAULT_TITLE = 'Website Feedback';
 
+const createDescription = ({ title, slug, siteUrl } = {}) => `## Description
+${title && siteUrl && slug ? `Page: [${title}](${siteUrl}${slug})\n` : ''}
+[NOTE]: # (Describe the problem you're encountering.)
+[TIP]: # (Do NOT give us access or passwords to your New Relic account or API keys!)
+
+## Steps to Reproduce
+
+[NOTE]: # (Please be as specific as possible.)
+
+## Expected Behavior
+
+[NOTE]: # (Tell us what you expected to happen.)
+
+## Relevant Logs / Console output
+
+[NOTE]: # (Please provide specifics of the local error logs, Browser Dev Tools console, etc. if appropriate and possible.)
+
+## Your Environment
+
+[TIP]: # (Include as many relevant details about your environment as possible.)
+[TIP]: # (ex: Browser name and version)
+[TIP]: # (ex: Operating System and version)
+
+## Additional context
+
+[TIP]: # (Add any other context about the problem here.)
+`;
+
 /**
  * Constructs a new issue url with information pre-filled for the user.
  *
@@ -10,17 +38,28 @@ const DEFAULT_TITLE = 'Website Feedback';
  * @param {string} page.title The title for the page.
  * @param {string} page.slug The slug for the page.
  * @param {string} page.siteUrl The base URL for the site.
+ * @param {string} [description] An (optional) description for the issue.
  * @returns {string} The URL for a new issue.
  */
-const createIssueURL = (repository, title, labels = [], page = {}) => {
+const createIssueURL = (
+  repository,
+  title,
+  labels = [],
+  page = {},
+  description
+) => {
   const baseURL = `${repository}/issues/new`;
 
   const params = new URLSearchParams();
   params.set('labels', labels.join(','));
   params.set('title', title || DEFAULT_TITLE);
 
-  if (page.title && page.slug && page.siteUrl) {
-    params.set('body', `Page: [${page.title}](${page.siteUrl}${page.slug})`);
+  if (description) {
+    params.set('body', description);
+  } else if (page.title && page.slug && page.siteUrl) {
+    params.set('body', createDescription(page));
+  } else {
+    params.set('body', createDescription());
   }
 
   return [baseURL, params.toString()].join('?');
