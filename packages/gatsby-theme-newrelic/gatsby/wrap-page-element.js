d4c2abab29a56134ef68c539fe4d742ed204793e
Jerel Miller <jerelmiller@gmail.com>
2021-01-06 15:33:24 -0800

    feat: wrap page element with initialized i18n instance and provider

--- packages/gatsby-theme-newrelic/gatsby/wrap-page-element.js
+++ packages/gatsby-theme-newrelic/gatsby/wrap-page-element.js
@@ -1,12 +1,51 @@
+/* global GATSBY_THEME_I18N_REACT_I18NEXT */
 /* eslint-disable react/prop-types */
 import React from 'react';
 import GlobalStyles from '../src/components/GlobalStyles';
+import i18n from 'i18next';
+import {
+  withDefaults,
+  themeNamespace,
+  themeSupportedLocales,
+} from '../src/utils/defaultOptions';
+import { I18nextProvider } from 'react-i18next';
 
-const wrapPageElement = ({ element }) => (
-  <>
-    <GlobalStyles />
-    {element}
-  </>
-);
+const wrapPageElement = ({ element }, themeOptions) => {
+  const {
+    i18n: { i18nextOptions },
+  } = withDefaults(themeOptions);
+
+  const locale = 'en';
+
+  const resources = i18nextOptions.ns
+    .filter((name) => name !== themeNamespace)
+    .reduce((resources, name) => {
+      const themeMessages = themeSupportedLocales.includes(locale)
+        ? require(`../src/i18n/translations/${locale}.json`)
+        : {};
+      const messages = require(`${GATSBY_THEME_I18N_REACT_I18NEXT}/${locale}/${name}.json`);
+
+      return {
+        ...resources,
+        en: {
+          ...resources.en,
+          [themeNamespace]: themeMessages,
+          [name]: messages,
+        },
+      };
+    }, {});
+
+  i18n.init({
+    ...i18n.i18nextOptions,
+    resources,
+  });
+
+  return (
+    <I18nextProvider i18n={i18n}>
+      <GlobalStyles />
+      {element}
+    </I18nextProvider>
+  );
+};
 
 export default wrapPageElement;
