429ea828fe91ea1f895f28343f4acabf8ca25f8b
Cayla Hamann <chamann@newrelic.com>
2021-01-11 13:47:14 -0500

    merge

--- packages/gatsby-theme-newrelic/gatsby/wrap-page-element.js
+++ packages/gatsby-theme-newrelic/gatsby/wrap-page-element.js
@@ -9,11 +9,28 @@ import {
   themeSupportedLocales,
 } from '../src/utils/defaultOptions';
 import { I18nextProvider } from 'react-i18next';
+import { useStaticQuery, graphql } from 'gatsby';
 
 const wrapPageElement = ({ element, props }, themeOptions) => {
   const { i18n: i18nConfig } = withDefaults(themeOptions);
   const locale = props.pageContext.locale || defaultLocale.locale;
 
+  console.log(props);
+  const {
+    allLocale: { nodes: locales },
+  } = useStaticQuery(graphql`
+    query {
+      allLocale {
+        nodes {
+          name
+          locale
+          localizedPath
+          isDefault
+        }
+      }
+    }
+  `);
+  console.log(locales);
   i18n.init({
     ...i18nConfig.i18nextOptions,
     lng: locale,
---
+++ packages/gatsby-theme-newrelic/src/components/SEO.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
+import { graphql, useStaticQuery } from 'gatsby';
+import useLocale from '../hooks/useLocale';
+
+const SEO = ({ title }) => {
+  return;
+};
+
+SEO.propTypes = {
+  title: PropTypes.string,
+};
+
+export default SEO;
