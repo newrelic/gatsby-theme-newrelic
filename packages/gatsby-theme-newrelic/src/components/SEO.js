b210d7d1530eece5eb38187ae05cc41a715749f8
Cayla Hamann <chamann@newrelic.com>
2021-01-11 19:52:13 -0500

    feat: SEO component added to theme and adds hreflang tags

--- packages/gatsby-theme-newrelic/src/components/SEO.js
+++ packages/gatsby-theme-newrelic/src/components/SEO.js
@@ -3,13 +3,61 @@ import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import { graphql, useStaticQuery } from 'gatsby';
 import useLocale from '../hooks/useLocale';
+const path = require('path');
 
-const SEO = ({ title }) => {
-  return;
+const SEO = ({ title, location }) => {
+  const {
+    site: { siteMetadata },
+    allLocale: { nodes: locales },
+  } = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          defaultTitle: title
+        }
+      }
+      allLocale {
+        nodes {
+          locale
+          localizedPath
+        }
+      }
+    }
+  `);
+
+  const currentLocale = useLocale();
+
+  const { defaultTitle } = siteMetadata;
+
+  const subPath =
+    currentLocale === 'en'
+      ? location.pathname
+      : location.pathname.replace(
+          new RegExp(`\\/${currentLocale.locale}`),
+          '/'
+        );
+
+  return (
+    <Helmet>
+      <title>{title || defaultTitle}</title>
+      {locales.map(({ locale, localizedPath }, i) => {
+        console.log(localizedPath);
+        return (
+          <link
+            key={i}
+            rel="alternate"
+            href={path.join(location.origin, localizedPath, subPath)}
+            hreflang={locale}
+          />
+        );
+      })}
+    </Helmet>
+  );
 };
 
 SEO.propTypes = {
   title: PropTypes.string,
+  location: PropTypes.string,
 };
 
 export default SEO;
