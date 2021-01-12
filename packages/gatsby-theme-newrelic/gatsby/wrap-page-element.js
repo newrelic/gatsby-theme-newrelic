6b2f2720495d035a4c698919643440a2aee17d48
Cayla Hamann <chamann@newrelic.com>
2021-01-11 19:57:19 -0500

    chore: revert wrapPageElement component

--- packages/gatsby-theme-newrelic/gatsby/wrap-page-element.js
+++ packages/gatsby-theme-newrelic/gatsby/wrap-page-element.js
@@ -9,29 +9,11 @@ import {
   themeSupportedLocales,
 } from '../src/utils/defaultOptions';
 import { I18nextProvider } from 'react-i18next';
-import { useStaticQuery, graphql } from 'gatsby';
-import LocaleProvider from '../src/components/LocaleProvider';
 
 const wrapPageElement = ({ element, props }, themeOptions) => {
   const { i18n: i18nConfig } = withDefaults(themeOptions);
-  const locale = getLocale(props, themeOptions);
+  const locale = props.pageContext.locale || defaultLocale.locale;
 
-  console.log(props);
-  const {
-    allLocale: { nodes: locales },
-  } = useStaticQuery(graphql`
-    query {
-      allLocale {
-        nodes {
-          name
-          locale
-          localizedPath
-          isDefault
-        }
-      }
-    }
-  `);
-  console.log(locales);
   i18n.init({
     ...i18nConfig.i18nextOptions,
     lng: locale,
@@ -40,32 +22,10 @@ const wrapPageElement = ({ element, props }, themeOptions) => {
 
   return (
     <I18nextProvider i18n={i18n}>
-      <LocaleProvider i18n={i18n}>
-        <GlobalStyles />
-        {element}
-      </LocaleProvider>
+      <GlobalStyles />
+      {element}
     </I18nextProvider>
   );
-};
-
-const getLocale = (props, themeOptions) => {
-  const { pageContext, location } = props;
-  const { i18n = {} } = themeOptions;
-
-  // For some reason we enter an infinite loop if we try and create localized
-  // 404 pages in gatsby-node.js. This is a hack that will use the current page
-  // location to find the locale to ensure the 404 page is translated
-  // accordingly.
-  if (!pageContext.fileRelativePath.match(/404/)) {
-    return pageContext.locale || defaultLocale.locale;
-  }
-
-  const [, base] = location.pathname.split('/');
-  const locale =
-    (i18n.additionalLocales || []).find(({ locale }) => locale === base) ||
-    defaultLocale;
-
-  return locale.locale;
 };
 
 const getResources = (i18nConfig, locale) => {
