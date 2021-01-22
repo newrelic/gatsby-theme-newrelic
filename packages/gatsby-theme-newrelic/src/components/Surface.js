5e056411ab79c6f0c80d2fc98517d853b8cb11ba
Jerel Miller <jerelmiller@gmail.com>
2021-01-22 00:52:29 -0800

    chore(Surface): box shadow tweaks

--- packages/gatsby-theme-newrelic/src/components/Surface.js
+++ packages/gatsby-theme-newrelic/src/components/Surface.js
@@ -32,22 +32,8 @@ const styles = {
 };
 
 const Surface = styled.div`
-  border-radius: 4px;
-  box-shadow: 0 0.24905px 0.55345px rgba(0, 0, 0, 0.00562291),
-    0 0.59851px 1.33002px rgba(0, 0, 0, 0.00807786),
-    0 1.12694px 2.50431px rgba(0, 0, 0, 0.01),
-    0 2.01027px 4.46726px rgba(0, 0, 0, 0.0119221),
-    0 3.75998px 8.35552px rgba(0, 0, 0, 0.0143771),
-    0 9px 20px rgba(0, 0, 0, 0.02);
-
-  .dark-mode & {
-    box-shadow: 0 1.66035px 2.10311px rgba(3, 15, 16, 0.0393604),
-      0 3.99006px 5.05408px rgba(3, 15, 16, 0.056545),
-      0 7.51293px 9.51638px rgba(3, 15, 16, 0.07),
-      0 13.4018px 16.9756px rgba(3, 15, 16, 0.083455),
-      0 25.0666px 31.751px rgba(3, 15, 16, 0.10064),
-      0 60px 76px rgba(3, 15, 16, 0.14);
-  }
+  border-radius: 0.25rem;
+  box-shadow: var(--shadow-3);
 
   ${({ base }) => styles.base[base]};
 
@@ -55,10 +41,12 @@ const Surface = styled.div`
     interactive &&
     css`
       cursor: pointer;
-      transition: transform 0.15s ease-out, border-color 0.15s ease-out;
+      transition: transform 0.15s ease-out, border-color 0.15s ease-out,
+        box-shadow 0.15s ease-out;
 
       &:hover {
         transform: translateY(-2px);
+        box-shadow: var(--shadow-4);
       }
 
       ${styles.interactive[base]}
