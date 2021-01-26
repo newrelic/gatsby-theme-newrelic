c33562255e5c0a7b7f99ae6a5dfe03bbce8ac578
Jerel Miller <jerelmiller@gmail.com>
2021-01-25 21:49:16 -0800

    chore(demo): source mdx files as pages

--- demo/gatsby-node.js
+++ demo/gatsby-node.js
@@ -1,3 +1,43 @@
+const path = require('path');
+
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const { createPage } = actions;
+
+  const { data, errors } = await graphql(`
+    query {
+      allMdx {
+        edges {
+          node {
+            frontmatter {
+              redirects
+            }
+            slug
+          }
+        }
+      }
+    }
+  `);
+
+  if (errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`);
+    return;
+  }
+
+  const { allMdx } = data;
+
+  allMdx.edges.forEach(({ node }) => {
+    const { slug } = node;
+
+    createPage({
+      path: slug,
+      component: path.resolve('src/templates/basic.js'),
+      context: {
+        slug,
+      },
+    });
+  });
+};
+
 exports.onCreatePage = ({ page, actions }) => {
   const { createPage, deletePage } = actions;
 
