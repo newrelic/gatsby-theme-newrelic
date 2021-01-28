9ecf1da36d2f5304b371c7dd864262e8955d84d4
Cayla Hamann <chamann@newrelic.com>
2021-01-28 17:32:48 -0500

    chore: fixed z-index to be lower

--- packages/gatsby-theme-newrelic/src/components/Backdrop.js
+++ packages/gatsby-theme-newrelic/src/components/Backdrop.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { css } from '@emotion/core';
-import Portal from './Portal';
+import { lighten } from 'polished';
 
 const Backdrop = ({ onClick }) => {
   return (
-    <Portal>
-      <div
-        css={css`
-          position: fixed;
-          top: 0;
-          left: 0;
-          right: 0;
-          bottom: 0;
-          background-color: var(--primary-background-color);
-          opacity: 0.75;
-          z-index: 10000;
-        `}
-        onClick={onClick}
-      ></div>
-    </Portal>
+    <div
+      css={css`
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background-color: ${lighten(0.2, '#1c2a2f')};
+        opacity: 0.75;
+        z-index: 100;
+      `}
+      onClick={onClick}
+    />
   );
 };
 
