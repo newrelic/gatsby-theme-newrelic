eca516b602d783ebb0a7cbf76a7cef6183b71c86
Jerel Miller <jerelmiller@gmail.com>
2021-02-02 21:20:59 -0800

    chore(demo): use relative links in code blocks

--- demo/src/pages/index.js
+++ demo/src/pages/index.js
@@ -43,8 +43,8 @@ const liveCodeSample = `
 
 const codeSampleWithAdditionalTags = `
 query AccountQuery(<var>$accountId:</var> ID!) {
-  <a href="https://docs.newrelic.com/docs/nerdgraph">account</a>(id: <var>$accountId</var>) {
-    <a href="https://docs.newrelic.com/docs/nrql"><var>name</var></a>
+  <a href="/build-apps">account</a>(id: <var>$accountId</var>) {
+    <a href="/build-apps/build-hello-world-app"><var>name</var></a>
   }
 }
 `;
