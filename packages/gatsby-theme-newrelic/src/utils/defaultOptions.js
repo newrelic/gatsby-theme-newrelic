e2a049d96c6a7a0727a8244bb533fd5c9f22653a
Jerel Miller <jerelmiller@gmail.com>
2021-02-03 18:31:09 -0800

    chore: add default set of site labels to theme config

--- packages/gatsby-theme-newrelic/src/utils/defaultOptions.js
+++ packages/gatsby-theme-newrelic/src/utils/defaultOptions.js
@@ -6,6 +6,21 @@ const themeSupportedLocales = ['en'];
 
 const uniq = (arr) => [...new Set(arr)];
 
+const DEFAULT_SITE_LABELS = {
+  'https://developer.newrelic.com': 'developer',
+  'https://opensource.newrelic.com': 'open source',
+  'https://docs.newrelic.com': 'docs',
+  'https://github.com': 'github',
+  'https://terraform.io': 'terraform',
+  'https://kubernetes.io': 'kubernetes',
+  'https://youtube.com': 'youtube',
+  'https://discuss.newrelic.com': 'discuss',
+  'https://blog.newrelic.com': 'blog',
+  'https://newrelic.com': 'newrelic.com',
+  'https://marketplace.visualstudio.com': 'visual studio',
+  'https://learn.newrelic.com': 'learn',
+};
+
 const withDefaults = (themeOptions) => {
   const { i18n = {}, relatedResources = {} } = themeOptions;
   const { i18nextOptions = {} } = i18n;
@@ -14,6 +29,10 @@ const withDefaults = (themeOptions) => {
     ...themeOptions,
     relatedResources: {
       ...relatedResources,
+      labels: {
+        ...DEFAULT_SITE_LABELS,
+        ...(relatedResources.labels || {}),
+      },
       swiftype: relatedResources.swiftype
         ? { limit: 5, refetch: false, ...relatedResources.swiftype }
         : false,
