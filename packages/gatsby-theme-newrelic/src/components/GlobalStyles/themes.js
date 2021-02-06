3ce3a23cd8b31eabd44013dc6bc3e5c13ddf2b66
Jerel Miller <jerelmiller@gmail.com>
2021-02-05 16:36:49 -0800

    fix: make link color a shade lighter in light mode

--- packages/gatsby-theme-newrelic/src/components/GlobalStyles/themes.js
+++ packages/gatsby-theme-newrelic/src/components/GlobalStyles/themes.js
@@ -13,8 +13,8 @@ export default css`
     --tertiary-background-color: var(--color-neutrals-200);
 
     --accent-text-color: var(--color-neutrals-500);
-    --link-color: var(--color-brand-600);
-    --link-hover-color: var(--color-brand-500);
+    --link-color: var(--color-brand-500);
+    --link-hover-color: var(--color-brand-300);
     --border-color: var(--color-neutrals-400);
     --border-hover-color: var(--color-neutrals-500);
     --divider-color: var(--color-neutrals-100);
