d2b7d56961ef7a3e3fe685a3be50ea8d0426e3e7
Clinton <clinton.langosch@gmail.com>
2021-02-10 11:15:20 -0800

    chore: add test for opening collapser via query param

--- packages/gatsby-theme-newrelic/src/components/__tests__/Collapser.test.js
+++ packages/gatsby-theme-newrelic/src/components/__tests__/Collapser.test.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { render, fireEvent, screen } from '@testing-library/react';
-import { LocationProvider } from '@reach/router';
+import {
+  createMemorySource,
+  createHistory,
+  LocationProvider,
+} from '@reach/router';
 import Collapser from '../Collapser';
+
+let history = createHistory(window);
+
+// for some types of tests you want a memory source
+let source = createMemorySource('/index?collapsers=open');
+let historyWithQueryParam = createHistory(source);
 
 test('hides collapser content by default', () => {
   render(
-    <LocationProvider>
+    <LocationProvider history={history}>
       <Collapser title="The title">The content inside the callout</Collapser>
     </LocationProvider>
   );
@@ -18,7 +28,7 @@ test('hides collapser content by default', () => {
 
 test('allows collapser to be open by default', () => {
   render(
-    <LocationProvider>
+    <LocationProvider history={history}>
       <Collapser defaultOpen title="The title">
         The content inside the callout
       </Collapser>
@@ -31,9 +41,22 @@ test('allows collapser to be open by default', () => {
   );
 });
 
+test('opens collapser via query param', () => {
+  render(
+    <LocationProvider history={historyWithQueryParam}>
+      <Collapser title="The title">The content inside the callout</Collapser>
+    </LocationProvider>
+  );
+
+  expect(screen.getByText(/The content/)).toHaveAttribute(
+    'aria-hidden',
+    'false'
+  );
+});
+
 test('opens the collapser by pressing the "s" key', () => {
   render(
-    <LocationProvider>
+    <LocationProvider history={history}>
       <Collapser title="The title">The content inside the callout</Collapser>
     </LocationProvider>
   );
@@ -48,7 +71,7 @@ test('opens the collapser by pressing the "s" key', () => {
 
 test('opens the collapser by pressing the "f" key', () => {
   render(
-    <LocationProvider>
+    <LocationProvider history={history}>
       <Collapser title="The title">The content inside the callout</Collapser>
     </LocationProvider>
   );
