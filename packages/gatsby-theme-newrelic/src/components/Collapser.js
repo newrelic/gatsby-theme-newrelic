12870a202cb63084fb02d221d24d485a71a3aec3
Clinton <clinton.langosch@gmail.com>
2021-02-10 10:17:05 -0800

    refactor: remove unnecessary useEffect require

--- packages/gatsby-theme-newrelic/src/components/Collapser.js
+++ packages/gatsby-theme-newrelic/src/components/Collapser.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useEffect } from 'react';
+import React, { useState, useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { css } from '@emotion/core';
 import Icon from './Icon';
