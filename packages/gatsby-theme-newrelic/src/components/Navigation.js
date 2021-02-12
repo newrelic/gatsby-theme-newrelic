99746cdc7de0e63ba8c1995d0eaa790ae4ddf0d7
Zack Stickles <zstickles@gmail.com>
2021-02-12 10:32:56 -0800

    chore: added context provider

--- packages/gatsby-theme-newrelic/src/components/Navigation.js
+++ packages/gatsby-theme-newrelic/src/components/Navigation.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
 import NavigationContext from './NavigationContext';
 
-const Navigation = ({ children, searchTerm }) => {
-  return <nav>{children}</nav>;
-};
+const Navigation = ({ children, searchTerm }) => (
+  <NavigationContext value={{ searchTerm }}>
+    <nav role="navigation" aria-label="Navigation">
+      {children}
+    </nav>
+  </NavigationContext>
+);
 
 Navigation.propTypes = {
   children: PropTypes.node,
