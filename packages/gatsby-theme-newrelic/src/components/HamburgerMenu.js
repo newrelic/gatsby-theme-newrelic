0a12364b8e32f3aedad62edc5f6d4bcbfc405677
Jerel Miller <jerelmiller@gmail.com>
2021-02-16 14:51:50 -0800

    fix: update HamburgerMenu design

--- packages/gatsby-theme-newrelic/src/components/HamburgerMenu.js
+++ packages/gatsby-theme-newrelic/src/components/HamburgerMenu.js
@@ -2,10 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { css } from '@emotion/core';
 
-const menuLine = (isOpen) => css`
+const menuLine = css`
   width: 100%;
   height: 2px;
-  background-color: var(--color-brand-600);
+  background-color: var(--color-neutrals-800);
   margin: 2px 0;
   border-radius: 5px;
   transition: 0.18s;
@@ -13,21 +13,6 @@ const menuLine = (isOpen) => css`
   .dark-mode & {
     background-color: var(--color-dark-800);
   }
-
-  ${isOpen &&
-  `
-  :nth-child(1) {
-    transform: rotate(-45deg) translate(-2px, 6.25px);
-  }
-
-  :nth-child(2) {
-    opacity: 0;
-    margin: 0
-  }
-
-  :nth-child(3) {
-    transform: rotate(45deg) translate(-2px, -6.25px);
-  }`}
 `;
 
 const HamburgerMenu = ({ onToggle, isOpen, className }) => (
@@ -55,9 +40,9 @@ const HamburgerMenu = ({ onToggle, isOpen, className }) => (
     className={className}
     onClick={() => onToggle()}
   >
-    <div css={menuLine(isOpen)} />
-    <div css={menuLine(isOpen)} />
-    <div css={menuLine(isOpen)} />
+    <div css={menuLine} />
+    <div css={menuLine} />
+    <div css={menuLine} />
   </button>
 );
 
