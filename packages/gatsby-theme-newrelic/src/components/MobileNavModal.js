456a1fc3a7b50c785ffb78e1cc820e6523541347
Jerel Miller <jerelmiller@gmail.com>
2021-02-16 22:04:40 -0800

    chore: render mobile nav modal in a portal

--- packages/gatsby-theme-newrelic/src/components/MobileNavModal.js
+++ packages/gatsby-theme-newrelic/src/components/MobileNavModal.js
@@ -5,6 +5,7 @@ import { animated } from 'react-spring';
 import { rgba } from 'polished';
 import { useScroll } from 'react-use';
 import Icon from './Icon';
+import Portal from './Portal';
 import useThemeTranslation from '../hooks/useThemeTranslation';
 
 const REM = 16;
@@ -17,95 +18,97 @@ const MobileNavModal = ({ children, style, onClose }) => {
   const shadowPoint = 0.5 * REM;
 
   return (
-    <animated.div
-      style={style}
-      css={css`
-        display: flex;
-        flex-direction: column;
-        border-radius: 0.5rem;
-        position: fixed;
-        top: 1rem;
-        bottom: 0.5rem;
-        left: 1rem;
-        right: 1rem;
-        z-index: 100;
-        transform-origin: top right;
-        box-shadow: var(--shadow-6);
-        background-color: white;
-        padding-bottom: 1rem;
-
-        .dark-mode & {
-          background: var(--color-dark-050);
-        }
-      `}
-    >
-      <header
+    <Portal>
+      <animated.div
+        style={style}
         css={css`
           display: flex;
-          justify-content: space-between;
-          align-items: center;
-          padding: 0.75rem 1rem;
-          border-bottom: 1px solid;
-          border-bottom-color: var(--divider-color);
-          box-shadow: ${y > shadowPoint ? 'var(--shadow-3)' : 'none'};
-          transition: 0.25s ease-out;
+          flex-direction: column;
+          border-radius: 0.5rem;
+          position: fixed;
+          top: 1rem;
+          bottom: 0.5rem;
+          left: 1rem;
+          right: 1rem;
+          z-index: 100;
+          transform-origin: top right;
+          box-shadow: var(--shadow-6);
+          background-color: white;
+          padding-bottom: 1rem;
 
           .dark-mode & {
-            border-bottom-color: ${y > shadowPoint
-              ? 'var(--color-dark-050)'
-              : rgba('#22353c', 0.4)};
+            background: var(--color-dark-050);
           }
         `}
       >
-        <span
+        <header
           css={css`
-            color: var(--accent-text-color);
-            text-transform: uppercase;
-            font-size: 0.875rem;
-          `}
-        >
-          {t('mobileNav.header')}
-        </span>
-        <button
-          type="button"
-          onClick={onClose}
-          css={css`
-            background: none;
-            color: currentColor;
-            border: none;
-            padding: 0.25rem;
-            border-radius: 0.25rem;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            padding: 0.75rem 1rem;
+            border-bottom: 1px solid;
+            border-bottom-color: var(--divider-color);
+            box-shadow: ${y > shadowPoint ? 'var(--shadow-3)' : 'none'};
+            transition: 0.25s ease-out;
 
-            &:active {
-              background: var(--color-neutrals-100);
-
-              .dark-mode & {
-                background: var(--color-dark-100);
-              }
+            .dark-mode & {
+              border-bottom-color: ${y > shadowPoint
+                ? 'var(--color-dark-050)'
+                : rgba('#22353c', 0.4)};
             }
           `}
         >
-          <Icon
-            name="fe-x"
-            size="1rem"
+          <span
             css={css`
-              display: block;
+              color: var(--accent-text-color);
+              text-transform: uppercase;
+              font-size: 0.875rem;
             `}
-          />
-        </button>
-      </header>
-      <div
-        ref={scrollRef}
-        css={css`
-          flex: 1;
-          padding: 1rem;
-          height: 100%;
-          overflow: auto;
-        `}
-      >
-        {children}
-      </div>
-    </animated.div>
+          >
+            {t('mobileNav.header')}
+          </span>
+          <button
+            type="button"
+            onClick={onClose}
+            css={css`
+              background: none;
+              color: currentColor;
+              border: none;
+              padding: 0.25rem;
+              border-radius: 0.25rem;
+
+              &:active {
+                background: var(--color-neutrals-100);
+
+                .dark-mode & {
+                  background: var(--color-dark-100);
+                }
+              }
+            `}
+          >
+            <Icon
+              name="fe-x"
+              size="1rem"
+              css={css`
+                display: block;
+              `}
+            />
+          </button>
+        </header>
+        <div
+          ref={scrollRef}
+          css={css`
+            flex: 1;
+            padding: 1rem;
+            height: 100%;
+            overflow: auto;
+          `}
+        >
+          {children}
+        </div>
+      </animated.div>
+    </Portal>
   );
 };
 
