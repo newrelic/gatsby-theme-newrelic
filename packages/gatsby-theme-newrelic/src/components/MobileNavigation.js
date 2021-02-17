df21f42335f50cefbc50f12cebffaff8ea3c544d
Jerel Miller <jerelmiller@gmail.com>
2021-02-16 23:10:18 -0800

    chore(MobileNavigation): close the navigation when changing pages

--- packages/gatsby-theme-newrelic/src/components/MobileNavigation.js
+++ packages/gatsby-theme-newrelic/src/components/MobileNavigation.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import MobileNavModal from './MobileNavModal';
+import usePrevious from '../hooks/usePrevious';
 import useScrollFreeze from '../hooks/useScrollFreeze';
+import { useLocation } from '@reach/router';
 import { useTransition } from 'react-spring';
 
 const MobileNavigation = ({ isOpen, children, onClose }) => {
   useScrollFreeze(isOpen);
+
+  const location = useLocation();
+  const previousLocation = usePrevious(location);
+  const hasChangedPage = location.pathname !== previousLocation?.pathname;
 
   const transitions = useTransition(isOpen, null, {
     config: { mass: 1, tension: 350, friction: 25, velocity: 10 },
@@ -17,6 +23,12 @@ const MobileNavigation = ({ isOpen, children, onClose }) => {
     enter: { opacity: 1, transform: 'scale(1)' },
     leave: { opacity: 0, transform: 'scale(0.92)' },
   });
+
+  useEffect(() => {
+    if (hasChangedPage) {
+      onClose();
+    }
+  }, [hasChangedPage, onClose]);
 
   return transitions.map(
     ({ item, key, props }) =>
