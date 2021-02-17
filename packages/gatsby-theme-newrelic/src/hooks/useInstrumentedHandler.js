3e27459540e96e45790a9f3fdaa6f9a2c3359970
Jerel Miller <jerelmiller@gmail.com>
2021-02-16 18:01:53 -0800

    feat: add a useInstrumentedHandler hook to easily instrument a handler with New Relic browser

---
+++ packages/gatsby-theme-newrelic/src/hooks/useInstrumentedHandler.js
@@ -0,0 +1,35 @@
+import { useCallback, useEffect, useRef } from 'react';
+import warning from 'warning';
+
+const useInstrumentedHandler = (handler, attributes) => {
+  const savedHandler = useRef();
+
+  useEffect(() => {
+    savedHandler.current = handler;
+  }, [handler]);
+
+  return useCallback(
+    (...args) => {
+      const { actionName, ...attrs } =
+        typeof attributes === 'function' ? attributes(...args) : attributes;
+
+      if (window.newrelic) {
+        warning(
+          actionName,
+          'You are attempting to instrument a handler, but the `actionName` property is not set. This will result in a no-op.'
+        );
+
+        if (actionName) {
+          actionName && window.newrelic.addPageAction(actionName, attrs);
+        }
+      }
+
+      if (savedHandler.current) {
+        return savedHandler.current(...args);
+      }
+    },
+    [attributes]
+  );
+};
+
+export default useInstrumentedHandler;
