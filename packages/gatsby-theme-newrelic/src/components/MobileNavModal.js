15e019cfe50b072a3b9e94818a47fdda21e73866
Jerel Miller <jerelmiller@gmail.com>
2021-02-16 21:49:27 -0800

    refactor(MobileNavigation): extract modal to own component and add box shadow on header when scrolled

---
+++ packages/gatsby-theme-newrelic/src/components/MobileNavModal.js
@@ -0,0 +1,118 @@
+import React, { useRef } from 'react';
+import PropTypes from 'prop-types';
+import { css } from '@emotion/core';
+import { animated } from 'react-spring';
+import { rgba } from 'polished';
+import { useScroll } from 'react-use';
+import Icon from './Icon';
+import useThemeTranslation from '../hooks/useThemeTranslation';
+
+const REM = 16;
+
+const MobileNavModal = ({ children, style, onClose }) => {
+  const scrollRef = useRef();
+  const { y } = useScroll(scrollRef);
+  const { t } = useThemeTranslation();
+
+  const shadowPoint = 0.5 * REM;
+
+  return (
+    <animated.div
+      style={style}
+      css={css`
+        display: flex;
+        flex-direction: column;
+        border-radius: 0.5rem;
+        position: fixed;
+        top: 1rem;
+        bottom: 0.5rem;
+        left: 1rem;
+        right: 1rem;
+        z-index: 100;
+        transform-origin: top right;
+        box-shadow: var(--shadow-6);
+        background-color: white;
+        padding-bottom: 1rem;
+
+        .dark-mode & {
+          background: var(--color-dark-050);
+        }
+      `}
+    >
+      <header
+        css={css`
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          padding: 0.75rem 1rem;
+          border-bottom: 1px solid;
+          border-bottom-color: var(--divider-color);
+          box-shadow: ${y > shadowPoint ? 'var(--shadow-3)' : 'none'};
+          transition: 0.25s ease-out;
+
+          .dark-mode & {
+            border-bottom-color: ${y > shadowPoint
+              ? 'var(--color-dark-050)'
+              : rgba('#22353c', 0.4)};
+          }
+        `}
+      >
+        <span
+          css={css`
+            color: var(--accent-text-color);
+            text-transform: uppercase;
+            font-size: 0.875rem;
+          `}
+        >
+          {t('mobileNav.header')}
+        </span>
+        <button
+          type="button"
+          onClick={onClose}
+          css={css`
+            background: none;
+            color: currentColor;
+            border: none;
+            padding: 0.25rem;
+            border-radius: 0.25rem;
+
+            &:active {
+              background: var(--color-neutrals-100);
+
+              .dark-mode & {
+                background: var(--color-dark-100);
+              }
+            }
+          `}
+        >
+          <Icon
+            name="fe-x"
+            size="1rem"
+            css={css`
+              display: block;
+            `}
+          />
+        </button>
+      </header>
+      <div
+        ref={scrollRef}
+        css={css`
+          flex: 1;
+          padding: 1rem;
+          height: 100%;
+          overflow: auto;
+        `}
+      >
+        {children}
+      </div>
+    </animated.div>
+  );
+};
+
+MobileNavModal.propTypes = {
+  children: PropTypes.node,
+  style: PropTypes.object.isRequired,
+  onClose: PropTypes.func.isRequired,
+};
+
+export default MobileNavModal;
--- packages/gatsby-theme-newrelic/src/components/MobileNavigation.js
+++ packages/gatsby-theme-newrelic/src/components/MobileNavigation.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Icon from './Icon';
-import { animated, useTransition } from 'react-spring';
-import { css } from '@emotion/core';
-import { rgba } from 'polished';
-import useThemeTranslation from '../hooks/useThemeTranslation';
+import MobileNavModal from './MobileNavModal';
+import { useTransition } from 'react-spring';
 
 const MobileNavigation = ({ isOpen, children, onClose }) => {
-  const { t } = useThemeTranslation();
   const transitions = useTransition(isOpen, null, {
     config: { mass: 1, tension: 350, friction: 25, velocity: 10 },
     from: {
@@ -22,91 +18,9 @@ const MobileNavigation = ({ isOpen, children, onClose }) => {
   return transitions.map(
     ({ item, key, props }) =>
       item && (
-        <animated.div
-          key={key}
-          style={props}
-          css={css`
-            display: flex;
-            flex-direction: column;
-            border-radius: 0.5rem;
-            position: fixed;
-            top: 1rem;
-            bottom: 0.5rem;
-            left: 1rem;
-            right: 1rem;
-            z-index: 100;
-            transform-origin: top right;
-            box-shadow: var(--shadow-6);
-            background-color: white;
-            padding-bottom: 1rem;
-
-            .dark-mode & {
-              background: var(--color-dark-050);
-            }
-          `}
-        >
-          <header
-            css={css`
-              display: flex;
-              justify-content: space-between;
-              align-items: center;
-              padding: 0.75rem 1rem;
-              border-bottom: 1px solid;
-              border-bottom-color: var(--divider-color);
-
-              .dark-mode & {
-                border-bottom-color: ${rgba('#22353c', 0.4)};
-              }
-            `}
-          >
-            <span
-              css={css`
-                color: var(--accent-text-color);
-                text-transform: uppercase;
-                font-size: 0.875rem;
-              `}
-            >
-              {t('mobileNav.header')}
-            </span>
-            <button
-              type="button"
-              onClick={onClose}
-              css={css`
-                background: none;
-                color: currentColor;
-                border: none;
-                padding: 0.25rem;
-                border-radius: 0.25rem;
-
-                &:active {
-                  background: var(--color-neutrals-100);
-
-                  .dark-mode & {
-                    background: var(--color-dark-100);
-                  }
-                }
-              `}
-            >
-              <Icon
-                name="fe-x"
-                size="1rem"
-                css={css`
-                  display: block;
-                `}
-              />
-            </button>
-          </header>
-          <div
-            css={css`
-              flex: 1;
-              padding: 1rem;
-              height: 100%;
-              overflow: auto;
-            `}
-          >
-            {children}
-          </div>
-        </animated.div>
+        <MobileNavModal key={key} style={props} onClose={onClose}>
+          {children}
+        </MobileNavModal>
       )
   );
 };
