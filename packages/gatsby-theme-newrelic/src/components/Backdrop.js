5815396e50a9f91d7779b28eb1394ca69a47e5a6
Jerel Miller <jerelmiller@gmail.com>
2021-02-24 17:41:24 -0800

    chore: update backdrop style

--- packages/gatsby-theme-newrelic/src/components/Backdrop.js
+++ packages/gatsby-theme-newrelic/src/components/Backdrop.js
@@ -1,28 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { animated } from 'react-spring';
 import { css } from '@emotion/core';
-import { lighten } from 'polished';
+import { darken, rgba } from 'polished';
 
-const Backdrop = ({ onClick }) => {
-  return (
-    <div
-      css={css`
-        position: fixed;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        background-color: ${lighten(0.1, '#1c2a2f')};
-        opacity: 0.75;
-        z-index: 100;
-      `}
-      onClick={onClick}
-    />
-  );
-};
+const Backdrop = ({ onClick, style }) => (
+  <animated.div
+    style={{ opacity: style?.opacity }}
+    css={css`
+      position: fixed;
+      top: 0;
+      left: 0;
+      bottom: 0;
+      right: 0;
+      z-index: 90;
+
+      background: ${rgba('#d5d7d7', 0.5)};
+
+      .dark-mode & {
+        background: ${rgba(darken(0.05, '#22353c'), 0.5)};
+      }
+    `}
+    onClick={onClick}
+  />
+);
 
 Backdrop.propTypes = {
   onClick: PropTypes.func,
+  style: PropTypes.shape({
+    opacity: PropTypes.number,
+  }),
 };
 
 export default Backdrop;
