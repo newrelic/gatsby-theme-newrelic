8fc51359a3ff262b20de1a67795bc54d57f4a334
Jerel Miller <jerelmiller@gmail.com>
2021-02-24 23:13:26 -0800

    fix: add fe-corner-down-left icon

--- packages/gatsby-theme-newrelic/src/icons/feather.js
+++ packages/gatsby-theme-newrelic/src/icons/feather.js
@@ -15,6 +15,12 @@ export default {
       <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1" />
     </>
   ),
+  'corner-down-left': (
+    <>
+      <polyline points="9 10 4 15 9 20" />
+      <path d="M20 4v7a4 4 0 0 1-4 4H4" />
+    </>
+  ),
   edit: (
     <>
       <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />
