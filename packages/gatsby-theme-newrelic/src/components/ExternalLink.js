fb3a3240722c6fe12dfba97e3499b79d0aaffc63
Cayla Hamann <chamann@newrelic.com>
2021-03-03 18:01:36 -0500

    chore: external link component uses link component instead

--- packages/gatsby-theme-newrelic/src/components/ExternalLink.js
+++ packages/gatsby-theme-newrelic/src/components/ExternalLink.js
@@ -1,66 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import useInstrumentedHandler from '../hooks/useInstrumentedHandler';
-import { localizeExternalLink } from '../utils/localization';
-import useLocale from '../hooks/useLocale';
-import useTessen from '../hooks/useTessen';
-import { useLocation } from '@reach/router';
+import Link from './Link';
 
-const isNewRelic = (to) => to.startsWith('https://newrelic.com');
-const isSignup = (to) => to.startsWith('https://newrelic.com/signup');
-
-const ExternalLink = ({
-  children,
-  onClick,
-  href,
-  instrumentation = {},
-  ...props
-}) => {
-  const locale = useLocale();
-  const tessen = useTessen();
-  const location = useLocation();
-  const properties =
-    typeof instrumentation === 'string'
-      ? { customProp: instrumentation }
-      : instrumentation;
-
-  const handleClick = useInstrumentedHandler(onClick, {
-    actionName: 'externalLink_click',
-    href: href,
-    ...properties,
-  });
-
-  const link = isNewRelic(href)
-    ? localizeExternalLink({ link: href, locale })
-    : href;
-
-  const trackSignUp = (event) => {
-    handleClick();
-    tessen.track('stitchedPathLinkClick', 'DocPageLinkClick', {
-      href: link,
-      path: location.pathname,
-      ...properties,
-    });
-  };
-
-  return (
-    <a
-      {...props}
-      href={link}
-      onClick={isSignup(href) ? trackSignUp : handleClick}
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      {children}
-    </a>
-  );
+const ExternalLink = ({ href, ...props }) => {
+  return <Link to={href} {...props} />;
 };
 
 ExternalLink.propTypes = {
-  children: PropTypes.node,
   href: PropTypes.string,
-  onClick: PropTypes.func,
-  instrumentation: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
 };
 
 export default ExternalLink;
