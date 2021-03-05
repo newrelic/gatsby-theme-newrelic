1eae230cbb946c5ddb87ca00cd42a6273d198eda
Jerel Miller <jerelmiller@gmail.com>
2021-03-04 17:00:03 -0800

    chore: create addTrailingSlash helper

--- packages/gatsby-theme-newrelic/src/utils/location.js
+++ packages/gatsby-theme-newrelic/src/utils/location.js
@@ -1,3 +1,7 @@
 export const stripTrailingSlash = (path) => path.replace(/\/$/, '');
+
 export const addLeadingSlash = (path) =>
   path.startsWith('/') ? path : `/${path}`;
+
+export const addTrailingSlash = (path) =>
+  path.endsWith('/') ? path : `${path}/`;
