0a0b8b2658e25dd54ad11dc7f781db6d7e943701
Clinton <clinton.langosch@gmail.com>
2021-03-10 16:39:22 -0600

    refactor(SEO): if not running on nr subdomain don't render meta tag at all

--- packages/gatsby-theme-newrelic/src/components/SEO.js
+++ packages/gatsby-theme-newrelic/src/components/SEO.js
@@ -41,7 +41,7 @@ const SEO = ({ title, location, children }) => {
   const getSwiftypeSiteType = () => {
     const nrSubDomain = /.*\.newrelic\.com/.test(location.hostname)
       ? location.hostname.split('.')[0]
-      : 'demo';
+      : null;
     const localeString = locale.isDefault ? '' : `-${locale.locale}`;
     return nrSubDomain ? nrSubDomain.concat(localeString) : null;
   };
