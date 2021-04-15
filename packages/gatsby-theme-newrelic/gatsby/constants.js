59bd5077f41b7d8902d077d7fed43679f6b4e5fa
Clinton <clinton.langosch@gmail.com>
2021-04-15 14:25:37 -0700

    refactor: remove gtm constants in favor of using site config

--- packages/gatsby-theme-newrelic/gatsby/constants.js
+++ packages/gatsby-theme-newrelic/gatsby/constants.js
@@ -5,7 +5,5 @@ const TESSEN_PATH = path.resolve(
   __dirname,
   `../static/tessen.min-${TESSEN_VERSION}.js`
 );
-const GA_PROPERTY_ID = 'UA-3047412-33';
-const GTAG_SRC = `https://www.googletagmanager.com/gtag/js`;
 
-module.exports = { TESSEN_PATH, GA_PROPERTY_ID, GTAG_SRC };
+module.exports = { TESSEN_PATH };
