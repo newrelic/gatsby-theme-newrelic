02dbb06b1d15854168f0655d1a214802fa71d532
Clinton <clinton.langosch@gmail.com>
2021-04-19 10:39:30 -0700

    refactor: remove unused gtm config defaults

--- packages/gatsby-theme-newrelic/src/utils/config/gtag.js
+++ packages/gatsby-theme-newrelic/src/utils/config/gtag.js
@@ -1,9 +1,7 @@
 const { buildConfigGetter } = require('../configBuilder');
 
 module.exports = buildConfigGetter('googleTagManager', {
-  envOptions: false,
   defaults: {
-    minify: process.env.NODE_ENV !== 'development',
     options: { anonymize_ip: true },
   },
 });
