6f9b8625f84c3558f5154354cea4fb234be11b1e
lbaker <lbaker@newrelic.com>
2021-05-13 12:04:31 -0700

    feat: Update feedback component functionality and tracking

--- packages/gatsby-theme-newrelic/src/components/SimpleFeedback.js
+++ packages/gatsby-theme-newrelic/src/components/SimpleFeedback.js
@@ -1,41 +1,23 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 import { css } from '@emotion/react';
 
 import Button from './Button';
-import GitHubIssueButton from './GitHubIssueButton';
 import Icon from './Icon';
 import PageTools from './PageTools';
 import useThemeTranslation from '../hooks/useThemeTranslation';
-import useInstrumentedHandler from '../hooks/useInstrumentedHandler';
+import useTessen from '../hooks/useTessen';
 
-const POSITIVE_ISSUE_BODY = `
-### Tell us more about your feedback
-
-We're glad to hear this doc was helpful! We've recorded your feedback. Help us 
-improve other content by telling us what worked and what didn't:
-
-* What worked well in this doc?
-* How could we make this doc even better?
-`;
-
-const NEGATIVE_ISSUE_BODY = `
-### Tell us more about your feedback
-
-Sorry to hear this doc wasn't helpful, but we'd love to make it better! We've 
-recorded your feedback. Help us improve this content by providing more info:
-
-* How can we improve this doc?
-`;
-
-const SimpleFeedback = ({ pageTitle, labels = [] }) => {
+const SimpleFeedback = () => {
+  const [clicked, setClicked] = useState(false);
   const { t } = useThemeTranslation();
-  const issueTitle = pageTitle ? `Feedback: ${pageTitle}` : 'Docs feedback';
+  const tessen = useTessen();
 
-  const handleClick = useInstrumentedHandler(null, (feedback) => ({
-    actionName: 'feedback_click',
-    feedback,
-  }));
+  const handleClick = (feedbackType) => {
+    setClicked(true);
+    tessen.track('feedbackThumbClick', `${feedbackType}FeedbackClick`, {
+      path: location.pathname,
+    });
+  };
 
   return (
     <PageTools.Section
@@ -54,69 +36,71 @@ const SimpleFeedback = ({ pageTitle, labels = [] }) => {
         }
       `}
     >
-      <h6
-        css={css`
-          margin-bottom: 0;
-        `}
-      >
-        {t('feedback.question')}
-      </h6>
-      <div
-        css={css`
-          display: flex;
-          justify-content: center;
-          align-items: flex-start;
-          gap: 0.25rem;
+      {clicked ? (
+        <h6
+          css={css`
+            padding: 0.3rem;
+          `}
+        >
+          Thank you for your feedback!
+        </h6>
+      ) : (
+        <>
+          {' '}
+          <h6
+            css={css`
+              margin-bottom: 0;
+            `}
+          >
+            {t('feedback.question')}
+          </h6>
+          <div
+            css={css`
+              display: flex;
+              justify-content: center;
+              align-items: flex-start;
+              gap: 0.25rem;
 
-          @supports not (gap: 0.5rem) {
-            a:first-of-type {
-              margin-right: 0.25rem;
-            }
-          }
-        `}
-      >
-        <GitHubIssueButton
-          labels={[...labels, 'feedback', 'feedback-positive']}
-          issueTitle={issueTitle}
-          issueBody={POSITIVE_ISSUE_BODY}
-          variant={Button.VARIANT.LINK}
-          size={Button.SIZE.EXTRA_SMALL}
-          onClick={() => handleClick('positive')}
-        >
-          <Icon
-            size="0.75rem"
-            name="fe-thumbsup"
-            css={css`
-              margin-right: 0.5rem;
+              @supports not (gap: 0.5rem) {
+                a:first-of-type {
+                  margin-right: 0.25rem;
+                }
+              }
             `}
-          />
-          {t('feedback.positive')}
-        </GitHubIssueButton>
-        <GitHubIssueButton
-          labels={[...labels, 'feedback', 'feedback-negative']}
-          issueTitle={issueTitle}
-          issueBody={NEGATIVE_ISSUE_BODY}
-          variant={Button.VARIANT.LINK}
-          size={Button.SIZE.EXTRA_SMALL}
-          onClick={() => handleClick('negative')}
-        >
-          <Icon
-            size="0.75rem"
-            name="fe-thumbsdown"
-            css={css`
-              margin-right: 0.5rem;
-            `}
-          />
-          {t('feedback.negative')}
-        </GitHubIssueButton>
-      </div>
+          >
+            <Button
+              variant={Button.VARIANT.LINK}
+              size={Button.SIZE.EXTRA_SMALL}
+              onClick={() => handleClick('Positive')}
+            >
+              <Icon
+                size="0.75rem"
+                name="fe-thumbsup"
+                css={css`
+                  margin-right: 0.5rem;
+                `}
+              />
+              {t('feedback.positive')}
+            </Button>
+            <Button
+              variant={Button.VARIANT.LINK}
+              size={Button.SIZE.EXTRA_SMALL}
+              onClick={() => handleClick('Negative')}
+            >
+              <Icon
+                size="0.75rem"
+                name="fe-thumbsdown"
+                css={css`
+                  margin-right: 0.5rem;
+                `}
+              />
+              {t('feedback.negative')}
+            </Button>
+          </div>
+        </>
+      )}
     </PageTools.Section>
   );
-};
-
-SimpleFeedback.propTypes = {
-  pageTitle: PropTypes.string,
-  labels: PropTypes.arrayOf(PropTypes.string),
 };
 
 export default SimpleFeedback;
