6e8163db34c8b10552aaeaa2dc61f15b924075b0
Clinton <clinton.langosch@gmail.com>
2021-09-16 16:20:44 -0700

    refactor: remove IO in header nav

--- packages/gatsby-theme-newrelic/src/components/GlobalHeader.js
+++ packages/gatsby-theme-newrelic/src/components/GlobalHeader.js
@@ -39,7 +39,6 @@ export const NR_SITES = {
   OSS: 'OSS',
   COMMUNITY: 'COMMUNITY',
   LEARN: 'LEARN',
-  IO: 'IO',
 };
 
 const HEADER_LINKS = new Map();
@@ -63,10 +62,6 @@ HEADER_LINKS.set(NR_SITES.DOCS, {
   .set(NR_SITES.LEARN, {
     text: 'Learn',
     href: 'https://learn.newrelic.com/',
-  })
-  .set(NR_SITES.IO, {
-    text: 'Instant Observability',
-    href: 'https://developer.newrelic.com/instant-observability',
   });
 
 const createNavList = (listType, activeSite = null) => {
