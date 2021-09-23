1009290dc42a067f1b538ee474d2c512c28316cd
Clinton <clinton.langosch@gmail.com>
2021-09-23 13:44:50 -0700

    chore: remove import for removed util

--- packages/gatsby-theme-newrelic/src/utils/config/index.js
+++ packages/gatsby-theme-newrelic/src/utils/config/index.js
@@ -2,6 +2,5 @@ module.exports = {
   getI18nConfig: require('./i18n'),
   getResolvedEnv: require('./resolvedEnv'),
   getTessenConfig: require('./tessen'),
-  getGtmConfig: require('./gtag'),
   getTrailingSlashesConfig: require('./trailingSlashes'),
 };
