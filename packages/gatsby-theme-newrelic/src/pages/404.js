84fcbcb7d0a33b95b30a02ebe2c27e8459e6baf3
Kris Kenney <70179215+nr-kkenney@users.noreply.github.com>
2021-10-20 15:24:26 -0700

    chore: split out search function from related resources

--- packages/gatsby-theme-newrelic/src/pages/404.js
+++ packages/gatsby-theme-newrelic/src/pages/404.js
@@ -2,12 +2,12 @@ import React, { useEffect, useState, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { css } from '@emotion/react';
 import { navigate } from 'gatsby-link';
+import fetch from 'node-fetch';
 
 import GlobalHeader from '../components/GlobalHeader';
 import GlobalFooter from '../components/GlobalFooter';
 import Link from '../components/Link';
 import SearchInput from '../components/SearchInput';
-import search from '../utils/related-resources/search';
 import Tag from '../components/Tag';
 import getLocale from '../../gatsby/utils/getLocale';
 import useThemeTranslation from '../hooks/useThemeTranslation';
@@ -48,41 +48,51 @@ const NotFoundPage = ({
       return pageLocale === 'en' ? '' : `-${pageLocale}`;
     };
 
-    if (searchTerm !== null) {
-      const results = await search(
-        location.origin,
+    const search = async () => {
+      const res = await fetch(
+        'https://search-api.swiftype.com/api/v1/public/engines/search.json',
         {
-          q: searchTerm,
-          filters: {
-            page: {
-              type: [
-                `docs${localePostFix()}`,
-                `developers${localePostFix()}`,
-                `opensource${localePostFix()}`,
-                `quickstarts${localePostFix()}`,
-              ],
-              document_type: [
-                '!views_page_menu',
-                '!term_page_api_menu',
-                '!term_page_landing_page',
-              ],
-            },
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
           },
-        },
-        {
-          engineKey: swiftypeEngineKey,
-          limit: 5,
-          excludedUrls: [],
+          body: JSON.stringify({
+            q: searchTerm,
+            engine_key: swiftypeEngineKey,
+            per_page: 5,
+            filters: {
+              page: {
+                type: [
+                  `docs${localePostFix()}`,
+                  `developers${localePostFix()}`,
+                  `opensource${localePostFix()}`,
+                  `quickstarts${localePostFix()}`,
+                ],
+                document_type: [
+                  '!views_page_menu',
+                  '!term_page_api_menu',
+                  '!term_page_landing_page',
+                ],
+              },
+            },
+          }),
         }
       );
 
+      const { records } = await res.json();
+
+      return records.page;
+    };
+
+    if (searchTerm !== null) {
+      const results = await search();
       const trimmedResults = results.map((r) => {
         return { url: r.url, title: r.title, type: r.type };
       });
 
       setSearchResult(trimmedResults);
     }
-  }, [pageLocale, searchTerm, location.origin, swiftypeEngineKey]);
+  }, [pageLocale, searchTerm, swiftypeEngineKey]);
 
   const displaySearchResults = (locale) => {
     if (!searchResult || searchResult.length === 0) {
