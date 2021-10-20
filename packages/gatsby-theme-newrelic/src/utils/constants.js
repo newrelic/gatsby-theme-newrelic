103ba87b25057c7cdfe97220380ffc21b570246e
Kris Kenney <70179215+nr-kkenney@users.noreply.github.com>
2021-10-20 13:52:29 -0700

    chore: fixup constants export

--- packages/gatsby-theme-newrelic/src/utils/constants.js
+++ packages/gatsby-theme-newrelic/src/utils/constants.js
@@ -1,22 +1,31 @@
 const prefixStorageKey = (name) => ['gatsby-theme-newrelic', name].join(':');
 
-export const SPLITS = {
+const SPLITS = {
   GLOBAL_HEADER_GITHUB_BUTTONS: 'developer-website_global-header-gh-buttons',
   FREE_ACCOUNT_BUTTON_COLOR: 'free_account_button_color',
 };
 
-export const SPLIT_TRACKING_EVENTS = {
+const SPLIT_TRACKING_EVENTS = {
   GLOBAL_HEADER_CLICK_ACTION: 'global_header.action_clicked',
   GLOBAL_HEADER_FREE_ACCOUNT_CLICK: 'global_header.free_account_button_click',
 };
 
-export const STORAGE_KEYS = {
+const STORAGE_KEYS = {
   USER_ID: prefixStorageKey('userId'),
   LAST_ANNOUNCEMENT_DISMISSED: prefixStorageKey('lastAnnouncementDismissed'),
 };
 
-export const TRACKING_COOKIE_NAME = 'newrelic-gdpr-consent';
+const TRACKING_COOKIE_NAME = 'newrelic-gdpr-consent';
 
-export const DEV_SEGMENT_WRITE_KEY = 'oMdv2YZCnzuC1iTVi9iCnFn6F9ycYb5v';
+const DEV_SEGMENT_WRITE_KEY = 'oMdv2YZCnzuC1iTVi9iCnFn6F9ycYb5v';
 
-export const SWIFTYPE_ENGINE_KEY = 'Ad9HfGjDw4GRkcmJjUut';
+const SWIFTYPE_ENGINE_KEY = 'Ad9HfGjDw4GRkcmJjUut';
+
+module.exports = {
+  SPLITS,
+  SPLIT_TRACKING_EVENTS,
+  STORAGE_KEYS,
+  TRACKING_COOKIE_NAME,
+  DEV_SEGMENT_WRITE_KEY,
+  SWIFTYPE_ENGINE_KEY,
+};
