e451789e2e0e60c925fa2f62ddf5183fca2f69f1
Tabatha Young <tyoung@newrelic.com>
2021-11-02 14:37:02 -0700

    chore: add a/b testing to signup button text

--- packages/gatsby-theme-newrelic/src/utils/constants.js
+++ packages/gatsby-theme-newrelic/src/utils/constants.js
@@ -11,9 +11,19 @@ const DEV_SEGMENT_WRITE_KEY = 'oMdv2YZCnzuC1iTVi9iCnFn6F9ycYb5v';
 
 const SWIFTYPE_ENGINE_KEY = 'Ad9HfGjDw4GRkcmJjUut';
 
+const SPLITS = {
+  SIGNUP_BUTTON_TEXT: 'deven_signupbutton_text',
+};
+
+const SPLIT_TRACKING_EVENTS = {
+  SIGNUP_BUTTON_CLICK: 'DEVEN_signuptext_click',
+};
+
 module.exports = {
   STORAGE_KEYS,
   TRACKING_COOKIE_NAME,
   DEV_SEGMENT_WRITE_KEY,
   SWIFTYPE_ENGINE_KEY,
+  SPLITS,
+  SPLIT_TRACKING_EVENTS,
 };
