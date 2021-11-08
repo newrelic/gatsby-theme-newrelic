b06b51a7aae8652721d981370ac930f37c284da8
aswanson-nr <aswanson@newrelic.com>
2021-11-08 15:40:45 -0800

    fix: get mocks working for unit tests

--- packages/gatsby-theme-newrelic/src/components/__tests__/SplitTextButton.test.js
+++ packages/gatsby-theme-newrelic/src/components/__tests__/SplitTextButton.test.js
@@ -1,97 +1,68 @@
 import React from 'react';
-import TestRenderer from 'react-test-renderer';
-import rerender from '@testing-library/react';
 import { SplitFactory } from '@splitsoftware/splitio-react';
 import SplitTextButton from '../SplitTextButton';
-import { renderWithProviders } from '../../test-utils/renderHelpers';
+import {  renderWithProviders} from '../../test-utils/renderHelpers';
 import { screen } from '@testing-library/react';
 
-import useThemeTranslation from '../../hooks/useThemeTranslation';
-import Button from '../Button';
 
-// jest.mock('../../hooks/useThemeTranslation', () =>
-//   jest.fn(() => ({ t: jest.fn((s) => s) }))
-// );
-
-// jest.mock('../Button', () => ({ children }) => <button>{children}</button>);
-
-// jest.mock('gatsby', () => ({
-//   __esModule: true,
-//   graphql: () => {},
-//   Link: ({ to, ...props }) => <a href={to} {...props} />,
-//   useStaticQuery: () => ({
-//     allLocale: {
-//       nodes: [
-//         {
-//           name: 'English',
-//           locale: 'en',
-//           localizedPath: '/en',
-//           isDefault: true,
-//         },
-//       ],
-//     },
-//     site: {
-//       siteMetadata: {
-//         siteUrl: 'https://github.com/foo/bar',
-//         repository: 'https://foobar.net',
-//       },
-//     },
-//     newRelicThemeConfig: {
-//       tessen: {
-//         product: 'foo',
-//         subproduct: 'foobar',
-//       },
-//     },
-//   }),
-// }));
-
-export const renderWithTranslation = (component, options) => {
-  i18n.init({
-    ...i18nextOptions,
-    lng: 'en',
-    resources: {
-      en: {
-        [themeNamespace]: translations,
-      },
-    },
-  });
-
-  return render(
-    <I18nextProvider i18n={i18n}>
-      <LocaleProvider i18n={i18n}>{component}</LocaleProvider>
-    </I18nextProvider>,
-    options
-  );
-};
+jest.mock('gatsby', () => ({
+ __esModule: true,
+ graphql: () => {},
+ Link: ({ to, ...props }) => <a href={to} {...props} />,
+ useStaticQuery: () => ({
+   allLocale: {
+     nodes: [
+       {
+         name: 'English',
+         locale: 'en',
+         localizedPath: '/en',
+         isDefault: true,
+       },
+     ],
+   },
+   site: {
+     siteMetadata: {
+       siteUrl: 'https://github.com/foo/bar',
+       repository: 'https://foobar.net',
+     },
+   },
+   newRelicThemeConfig: {
+     tessen: {
+       product: 'foo',
+       subproduct: 'foobar',
+     },
+   },
+ }),
+}));
 
 test('renders default button text with no split', async () => {
-  const config = createSplitConfig('start_now');
-  //   console.log(config);
+  const config = createSplitConfig();
 
-  const element = renderWithProviders(
+  renderWithProviders(
     <SplitFactory config={config} updateOnSdkTimedout={true}>
       <SplitTextButton />
     </SplitFactory>
   );
 
-  await TestRenderer.act(async () => {
-    rerender(element);
-  });
-
-  expect(screen.getByText('Free account')).toBeTruthy();
+  expect(screen.queryByText('Start now')).not.toBeInTheDocument();
+  expect(await screen.findByText('Free account')).toBeInTheDocument();
 });
 
 const createSplitConfig = (text) => {
+  let treatment = {};
+  if (text) {
+    treatment = (text === 'start_now')
+      ? { treatment: 'start_now', config: '{ "text": "Start now" }' }
+      : { treatment: 'free_account', config: '{ "text": "Free account" }' };
+  }
+
   return {
     core: {
       authorizationKey: 'localhost',
       key: 'user',
     },
     features: {
-      deven_signupbutton_text:
-        text === 'start_now'
-          ? { treatment: 'start_now', config: '{ "text": "Start now" }' }
-          : { treatment: 'free_account', config: '{ "text": "Free account" }' },
+      deven_signupbutton_text: treatment
     },
     scheduler: {
       offlineRefreshRate: 15,
