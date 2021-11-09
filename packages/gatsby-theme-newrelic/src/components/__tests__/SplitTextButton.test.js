d05896f40892c7f252d40581cdd2a316eafcf9a5
aswanson-nr <aswanson@newrelic.com>
2021-11-08 16:01:37 -0800

    chore: add more test cases for signup button

--- packages/gatsby-theme-newrelic/src/components/__tests__/SplitTextButton.test.js
+++ packages/gatsby-theme-newrelic/src/components/__tests__/SplitTextButton.test.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { SplitFactory } from '@splitsoftware/splitio-react';
 import SplitTextButton from '../SplitTextButton';
-import {  renderWithProviders} from '../../test-utils/renderHelpers';
+import { renderWithProviders } from '../../test-utils/renderHelpers';
 import { screen } from '@testing-library/react';
-
 
 jest.mock('gatsby', () => ({
  __esModule: true,
@@ -44,8 +43,57 @@ test('renders default button text with no split', async () => {
     </SplitFactory>
   );
 
-  expect(screen.queryByText('Start now')).not.toBeInTheDocument();
   expect(await screen.findByText('Free account')).toBeInTheDocument();
+  expect(screen.queryByText('Start now')).not.toBeInTheDocument();
+});
+
+test('renders start_now treatment', async () => {
+  const config = createSplitConfig('start_now');
+
+  renderWithProviders(
+    <SplitFactory config={config} updateOnSdkTimedout={true}>
+      <SplitTextButton />
+    </SplitFactory>
+  );
+
+  expect(await screen.findByText('Start now')).toBeInTheDocument();
+  expect(screen.queryByText('Free account')).not.toBeInTheDocument();
+});
+
+test('renders free account treatment', async () => {
+  const config = createSplitConfig('free_account');
+
+  renderWithProviders(
+    <SplitFactory config={config} updateOnSdkTimedout={true}>
+      <SplitTextButton />
+    </SplitFactory>
+  );
+
+  expect(await screen.findByText('Free account')).toBeInTheDocument();
+  expect(screen.queryByText('Start now')).not.toBeInTheDocument();
+});
+
+test('renders if no features are returned from Split.io', async () => {
+  const config = {
+    core: {
+      authorizationKey: 'localhost',
+      key: 'user',
+    },
+    features: {},
+    scheduler: {
+      offlineRefreshRate: 15,
+    },
+    debug: false,
+
+  }
+  renderWithProviders(
+    <SplitFactory config={config} updateOnSdkTimedout={true}>
+      <SplitTextButton />
+    </SplitFactory>
+  );
+
+  expect(await screen.findByText('Free account')).toBeInTheDocument();
+  expect(screen.queryByText('Start now')).not.toBeInTheDocument();
 });
 
 const createSplitConfig = (text) => {
