52751bf655fb87c2e18375476b8f0a2cb865a3c7
Alec Swanson <70179303+aswanson-nr@users.noreply.github.com>
2021-11-09 16:10:50 -0800

    Revert "feat: add sitelink script for docs home page"

--- packages/gatsby-theme-newrelic/src/components/SEO.js
+++ packages/gatsby-theme-newrelic/src/components/SEO.js
@@ -4,17 +4,6 @@ import Helmet from 'react-helmet';
 import { graphql, useStaticQuery } from 'gatsby';
 import useLocale from '../hooks/useLocale';
 import path from 'path';
-import { SITE_LINKS_SCRIPTS } from '../utils/constants';
-
-const siteLinkScript = (location, title) => {
-  const { pathname } = location;
-  const homepage = '/';
-  if (pathname === homepage && SITE_LINKS_SCRIPTS[title]) {
-    return (
-      <script type="application/ld+json">{SITE_LINKS_SCRIPTS[title]}</script>
-    );
-  }
-};
 
 const SEO = ({ title, location, type, children }) => {
   const {
@@ -89,7 +78,6 @@ const SEO = ({ title, location, type, children }) => {
           content={getSwiftypeSiteType()}
         />
       )}
-      {siteLinkScript(location, title)}
       {children}
     </Helmet>
   );
