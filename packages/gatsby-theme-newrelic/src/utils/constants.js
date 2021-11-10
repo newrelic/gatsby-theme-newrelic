5031e19c87534611b18394ac6447934b8c2a61fc
lbaker <lbaker@newrelic.com>
2021-11-09 16:34:34 -0800

    chore: Remove const

--- packages/gatsby-theme-newrelic/src/utils/constants.js
+++ packages/gatsby-theme-newrelic/src/utils/constants.js
@@ -11,26 +11,9 @@ const DEV_SEGMENT_WRITE_KEY = 'oMdv2YZCnzuC1iTVi9iCnFn6F9ycYb5v';
 
 const SWIFTYPE_ENGINE_KEY = 'Ad9HfGjDw4GRkcmJjUut';
 
-const SITE_LINKS_SCRIPTS = {
-  'New Relic Documentation': `{
-    '@context': 'https://schema.org',
-    '@type': 'WebSite',
-    url: 'https://docs.newrelic.com/',
-    potentialAction: {
-      '@type': 'SearchAction',
-      target: {
-        '@type': 'EntryPoint',
-        urlTemplate: 'https://docs.newrelic.com/?q={search_term_string}',
-      },
-      'query-input': 'required name=search_term_string',
-    },
-  }`,
-};
-
 module.exports = {
   STORAGE_KEYS,
   TRACKING_COOKIE_NAME,
   DEV_SEGMENT_WRITE_KEY,
   SWIFTYPE_ENGINE_KEY,
-  SITE_LINKS_SCRIPTS,
 };
