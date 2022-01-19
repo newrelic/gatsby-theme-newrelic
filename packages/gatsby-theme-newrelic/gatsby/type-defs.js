c6e8f972b554c6b5104fb19eb7dda7d3be04fbd3
Joe Gregory <jgregory@newrelic.com>
2022-01-19 11:51:27 -0800

    chore: remove toString redundency in typedefs

--- packages/gatsby-theme-newrelic/gatsby/type-defs.js
+++ packages/gatsby-theme-newrelic/gatsby/type-defs.js
@@ -1,12 +1,11 @@
 const fs = require('fs');
 const path = require('path');
 
-const SCHEMA_CUSTOMIZATION_TYPES = fs
-  .readFileSync(path.resolve(__dirname, './type-defs.graphql'), {
+const SCHEMA_CUSTOMIZATION_TYPES = fs.readFileSync(
+  path.resolve(__dirname, './type-defs.graphql'),
+  {
     encoding: 'utf-8',
-  })
-
-  // converted toString for type-creation in gatsby-node
-  .toString();
+  }
+);
 
 module.exports = { SCHEMA_CUSTOMIZATION_TYPES };
