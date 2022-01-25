a2bc4e47f8f9ed13c735fd024b7315d39eb83e47
Joe Gregory <jgregory@newrelic.com>
2022-01-25 11:00:54 -0800

    wip: move typedefs back into constant

--- packages/gatsby-theme-newrelic/gatsby-node.js
+++ packages/gatsby-theme-newrelic/gatsby-node.js
@@ -112,7 +112,63 @@ exports.onPreBootstrap = ({ reporter, store }, themeOptions) => {
 exports.createSchemaCustomization = ({ actions }) => {
   const { createTypes } = actions;
 
-  createTypes(SCHEMA_CUSTOMIZATION_TYPES);
+  const typedefs = `
+  type SiteLayout @dontInfer {
+    contentPadding: String
+    maxWidth: String
+    mobileBreakpoint: String
+  }
+
+  type MdxFrontmatter @infer {
+    startDate: Date @dateformat(formatString: "YYYY-MM-DD")
+    endDate: Date @dateformat(formatString: "YYYY-MM-DD")
+  }
+
+  type SiteSiteMetadata {
+    repository: String
+    branch: String!
+    contributingUrl: String
+    title: String
+    titleTemplate: String
+  }
+
+  type Locale implements Node @dontInfer {
+    name: String!
+    localName: String!
+    locale: String!
+    hrefLang: String!
+    isDefault: Boolean!
+  }
+
+  type RelatedResource implements Node {
+    id: ID!
+    title: String!
+    url: String!
+  }
+
+  type NewRelicThemeConfig implements Node {
+    env: String!
+    relatedResources: NewRelicThemeRelatedResourceConfig!
+    tessen: NewRelicThemeTessenConfig
+  }
+
+  type NewRelicThemeRelatedResourceConfig {
+    labels: [RelatedResourceLabel!]!
+  }
+
+  type RelatedResourceLabel {
+    baseUrl: String!
+    label: String!
+  }
+
+  type NewRelicThemeTessenConfig {
+    product: String
+    subproduct: String
+  }
+
+  `;
+
+  createTypes(typedefs);
 };
 
 exports.sourceNodes = (
