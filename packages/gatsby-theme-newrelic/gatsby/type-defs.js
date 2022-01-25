51e26c9abf4d5709fd2086d1f0130f1a5bbfbd54
Joe Gregory <jgregory@newrelic.com>
2022-01-25 11:08:49 -0800

    chore: type-defs return a constant: removal of

--- packages/gatsby-theme-newrelic/gatsby/type-defs.js
+++ packages/gatsby-theme-newrelic/gatsby/type-defs.js
@@ -1,11 +1,56 @@
-const fs = require('fs');
-const path = require('path');
+const SCHEMA_CUSTOMIZATION_TYPES = ` gql
+type SiteLayout @dontInfer {
+  contentPadding: String
+  maxWidth: String
+  mobileBreakpoint: String
+}
 
-const SCHEMA_CUSTOMIZATION_TYPES = fs.readFileSync(
-  path.resolve(__dirname, './type-defs.graphql'),
-  {
-    encoding: 'utf-8',
-  }
-);
+type MdxFrontmatter @infer {
+  startDate: Date @dateformat(formatString: "YYYY-MM-DD")
+  endDate: Date @dateformat(formatString: "YYYY-MM-DD")
+}
+
+type SiteSiteMetadata {
+  repository: String
+  branch: String!
+  contributingUrl: String
+  title: String
+  titleTemplate: String
+}
+
+type Locale implements Node @dontInfer {
+  name: String!
+  localName: String!
+  locale: String!
+  hrefLang: String!
+  isDefault: Boolean!
+}
+
+type RelatedResource implements Node {
+  id: ID!
+  title: String!
+  url: String!
+}
+
+type NewRelicThemeConfig implements Node {
+  env: String!
+  relatedResources: NewRelicThemeRelatedResourceConfig!
+  tessen: NewRelicThemeTessenConfig
+}
+
+type NewRelicThemeRelatedResourceConfig {
+  labels: [RelatedResourceLabel!]!
+}
+
+type RelatedResourceLabel {
+  baseUrl: String!
+  label: String!
+}
+
+type NewRelicThemeTessenConfig {
+  product: String
+  subproduct: String
+}
+`;
 
 module.exports = { SCHEMA_CUSTOMIZATION_TYPES };
