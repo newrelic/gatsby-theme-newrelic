3f90e9e9ac1615ad48a185feaf6874c83feaf50f
lbaker <lbaker@newrelic.com>
2022-06-07 13:40:49 -0700

    chore: Add close button, remove esc functionality

--- packages/gatsby-theme-newrelic/src/components/Lightbox.js
+++ packages/gatsby-theme-newrelic/src/components/Lightbox.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { animated, useTransition } from 'react-spring';
-import { rgba } from 'polished';
 import { css } from '@emotion/react';
 import Portal from './Portal';
-import useKeyPress from '../hooks/useKeyPress';
+import Icon from './Icon';
 import useScrollFreeze from '../hooks/useScrollFreeze';
+import Button from './Button';
 
 const Lightbox = ({ children }) => {
   const [lightboxOpen, setLightboxOpen] = useState(false);
@@ -19,9 +19,6 @@ const Lightbox = ({ children }) => {
     leave: { opacity: 0, transform: 'scale(0.96)' },
   });
   useScrollFreeze(lightboxOpen);
-  useKeyPress('Escape', () => setLightboxOpen(false), {
-    ignoreTextInput: false,
-  });
 
   return (
     <>
@@ -59,21 +56,41 @@ const Lightbox = ({ children }) => {
                     justify-content: center;
                     align-items: center;
                     z-index: 900;
-                    background: ${rgba('#d5d7d7', 0.75)};
-
-                    img {
-                      max-height: 90vh;
-                      max-width: 90%;
-                      width: auto;
-                    }
-
-                    .dark-mode & {
-                      background: ${rgba('#3a444b', 0.75)};
-                    }
+                    background: var(--modal-wrapper-color);
                   `}
                   onClick={() => setLightboxOpen(false)}
                 >
-                  {children}
+                  <div
+                    css={css`
+                      max-height: 90vh;
+                      max-width: 90%;
+                      width: auto;
+                      display: flex;
+                      flex-direction: column;
+                      position: relative;
+                    `}
+                  >
+                    <Button
+                      type="button"
+                      css={css`
+                        position: absolute;
+                        top: -1rem;
+                        right: -1rem;
+                        align-self: flex-end;
+                        background: var(--secondary-background-color);
+                        color: var(--primary-text-color);
+                        border: var(--border-color) solid 2px;
+                        border-radius: 50%;
+                        padding: 0.25rem;
+                        display: flex;
+                        align-items: center;
+                      `}
+                      onClick={() => setLightboxOpen(false)}
+                    >
+                      <Icon name="fe-x" size="1.5rem" />
+                    </Button>
+                    {children}
+                  </div>
                 </animated.div>
               </Portal>
             )
