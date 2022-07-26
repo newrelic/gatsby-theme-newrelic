712e49f3dd9af7ddf6a38c18ccf22eecb6e6e93f
lbaker <lbaker@newrelic.com>
2022-07-26 14:32:48 -0700

    chore: Make InteractiveForm a dynamic wrapper

--- packages/gatsby-theme-newrelic/src/components/InteractiveForm.js
+++ packages/gatsby-theme-newrelic/src/components/InteractiveForm.js
@@ -1,56 +1,10 @@
-import React, { useState } from 'react';
-import { useDebounce } from 'react-use';
+import React from 'react';
 import { css } from '@emotion/react';
-import { scroller } from 'react-scroll';
-import Link from './Link';
+import PropTypes from 'prop-types';
 import Callout from './Callout';
-import CustomTextInput from './CustomTextInput';
 import InteractiveOutput from './InteractiveOutput';
 
-const InteractiveForm = () => {
-  const [appName, setAppName] = useState('My Application');
-  const [licenseKey, setLicenseKey] = useState('12345');
-
-  useDebounce(
-    () => {
-      if (appName !== 'My Application') {
-        scrollToAppName();
-      }
-    },
-    400,
-    [appName]
-  );
-
-  useDebounce(
-    () => {
-      if (licenseKey !== '12345') {
-        scrollToLicenseKey();
-      }
-    },
-    400,
-    [licenseKey]
-  );
-
-  const scrollToAppName = () => {
-    scroller.scrollTo('line-32', {
-      duration: 600,
-      delay: 0,
-      smooth: 'easeInOutQuart',
-      containerId: 'codeblock',
-      offset: -5,
-    });
-  };
-
-  const scrollToLicenseKey = () => {
-    scroller.scrollTo('line-16', {
-      duration: 600,
-      delay: 0,
-      smooth: 'easeInOutQuart',
-      containerId: 'codeblock',
-      offset: -5,
-    });
-  };
-
+const InteractiveForm = ({ children, inputs, config }) => {
   return (
     <div
       css={css`
@@ -71,55 +25,11 @@ const InteractiveForm = () => {
           }
         `}
       >
-        <CustomTextInput
-          name="app-name"
-          label="Name your app"
-          value={appName}
-          onChange={(e) => {
-            setAppName(e.target.value);
-          }}
-          toolTip="The app name in the agent's configuration file will be used in the New Relic user interface"
-          css={css`
-            margin-bottom: 1.5rem;
-          `}
-        />
-        <CustomTextInput
-          name="license-key"
-          label="Enter your New Relic"
-          value={licenseKey}
-          onChange={(e) => {
-            setLicenseKey(e.target.value);
-          }}
-          url={{
-            href: 'https://docs.newrelic.com/docs/apis/intro-apis/new-relic-api-keys/#ingest-license-key',
-            title: 'license key',
-          }}
-          css={css`
-            margin-bottom: 1.5rem;
-          `}
-        />
-        <Callout variant={Callout.VARIANT.TIP}>
-          When using the config file we recommend:
-          <ul>
-            <li>
-              change the default <code>newrelic.yml</code> file permissions to
-              read/write only for the owner of the application server process.
-            </li>
-            <li>
-              make sure <code>newrelic.yml</code> is part of your backup
-              routine.
-            </li>
-            <li>
-              use the{' '}
-              <Link to="https://docs.newrelic.com/docs/new-relic-solutions/solve-common-issues/diagnostics-cli-nrdiag/diagnostics-cli-nrdiag/">
-                New Relic Diagnostics CLI
-              </Link>{' '}
-              to validate your settings, either before or after you deploy.
-            </li>
-          </ul>
-        </Callout>
+        {children}
       </div>
       <InteractiveOutput
+        inputs={inputs}
+        config={config}
         css={css`
           margin-top: 1rem;
           width: 49%;
@@ -141,11 +51,34 @@ const InteractiveForm = () => {
             }
           }
         `}
-        appName={appName}
-        licenseKey={licenseKey}
       />
     </div>
   );
+};
+
+const Tip = ({ children }) => {
+  return <Callout variant={Callout.VARIANT.TIP}>{children}</Callout>;
+};
+
+const InputList = ({ children }) => {
+  return children;
+};
+
+InteractiveForm.Tip = Tip;
+InteractiveForm.InputList = InputList;
+
+InteractiveForm.propTypes = {
+  children: PropTypes.node,
+  inputs: PropTypes.array,
+  config: PropTypes.string,
+};
+
+Tip.propTypes = {
+  children: PropTypes.node,
+};
+
+InputList.propTypes = {
+  children: PropTypes.node,
 };
 
 export default InteractiveForm;
