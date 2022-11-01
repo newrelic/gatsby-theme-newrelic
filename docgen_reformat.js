74f6df627affb5dcc259717fc041ca2735d92cc4
echilds <echilds@newrelic.com>
2022-11-01 13:43:45 -0700

    chore: Write products of docgen_reformat to new json file

--- docgen_reformat.js
+++ docgen_reformat.js
@@ -1,20 +1,33 @@
 const fs = require('fs');
 
-const reformatted_data = JSON.parse(fs.readFileSync('./demo/docgen_output.json'));
+const reformatted_data = JSON.parse(
+  fs.readFileSync('./demo/docgen_output.json')
+);
 const docgen_values = Object.values(reformatted_data);
 
 const docgenUpdatedProps = docgen_values.map((item) => {
-
-  if(item.props) {
-    let newProps = item.props;
+  if (item.props) {
+    const newProps = item.props;
     const reduceProps = Object.entries(newProps).reduce((acc, val) => {
-        acc = [...acc, { name: val[0], type: val[1].type.name, required: val[1].required, description: val[1].description}]
-      return acc
-  }, []);
-  item.props = reduceProps
-  return item
+      acc = [
+        ...acc,
+        {
+          name: val[0],
+          type: val[1].type.name,
+          required: val[1].required,
+          description: val[1].description,
+        },
+      ];
+      return acc;
+    }, []);
+    item.props = reduceProps;
+    return item;
   } else {
-    return item
+    return item;
   }
-})
+});
 
+fs.writeFileSync(
+  './demo/src/docs/documentation.json',
+  JSON.stringify(docgenUpdatedProps)
+);
