900cd32433f405b9975dac6ec8cbf38e6e412da2
Liz Baker <lbaker@newrelic.com>
2023-01-18 16:13:39 -0800

    feat: Add TutorialStep components

---
+++ packages/gatsby-theme-newrelic/src/components/TutorialSteps/TutorialStep.js
@@ -0,0 +1,101 @@
+import React, { Children } from 'react';
+import PropTypes from 'prop-types';
+import { css } from '@emotion/react';
+import { isMdxType } from '../../utils/mdx';
+
+const ALLOWED_HEADINGS = ['h2', 'h3', 'h4'];
+
+const TutorialStep = ({ children, stepNumber, totalSteps }) => {
+  children = Children.toArray(children);
+
+  const title = ALLOWED_HEADINGS.some((level) => isMdxType(children[0], level))
+    ? children[0]
+    : null;
+
+  const content = title
+    ? children.filter((child) => child !== title)
+    : children;
+
+  return (
+    <div
+      css={css`
+        padding: 1.5rem 0 2rem 0;
+        border-top: 1px solid var(--divider-color);
+
+        &:last-child {
+          border-bottom: 1px solid var(--divider-color);
+        }
+      `}
+    >
+      <StepCounter stepNumber={stepNumber} total={totalSteps} />
+      {title}
+      {content.map((column, idx) => {
+        return Array.isArray(column) ? (
+          <div
+            key={idx}
+            css={css`
+              display: grid;
+              grid-template-columns: repeat(2, calc(50% - 0.5rem));
+              grid-gap: 1rem;
+
+              &:not(:last-child) {
+                margin-bottom: 2rem;
+              }
+            `}
+          >
+            <div>{column[0]}</div>
+            <div>{column[1]}</div>
+          </div>
+        ) : (
+          column
+        );
+      })}
+    </div>
+  );
+};
+
+TutorialStep.propTypes = {
+  children: PropTypes.node,
+  stepNumber: PropTypes.number.isRequired,
+  totalSteps: PropTypes.number.isRequired,
+};
+
+const StepCounter = ({ className, stepNumber, total }) => (
+  <div
+    className={className}
+    css={css`
+      --accent-size: 1.2em;
+
+      font-size: 0.75rem;
+      font-weight: 600;
+      color: var(--accent-text-color);
+      text-transform: uppercase;
+      margin-bottom: 0.25rem;
+    `}
+  >
+    Step{' '}
+    <span
+      css={css`
+        font-size: var(--accent-size);
+      `}
+    >
+      {stepNumber}
+    </span>{' '}
+    of{' '}
+    <span
+      css={css`
+        font-size: var(--accent-size);
+      `}
+    >
+      {total}
+    </span>
+  </div>
+);
+
+StepCounter.propTypes = {
+  className: PropTypes.string,
+  stepNumber: PropTypes.number,
+  total: PropTypes.number,
+};
+
+export default TutorialStep;
