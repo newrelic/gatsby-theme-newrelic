0cbdb30e7f91bab6f1987e9304d1f083be49e854
Sunny Zanchi <sunnyz@hey.com>
2023-08-30 14:35:12 -0400

    style fix: switch from `css` to `styled`
    as part of the upgrade to Gatsby 5, the `css` prop wasn't working on
    styled components or regular JSX tags, but _only_ in templates.
    regular pages are fine.
    i spent days trying to figure out wtf was going on and wasn't able to
    
    Liz figured out that switching from the `css` prop to `styled` works,
    so instead of toiling away at a heap of config files, we can just do this

--- demo/src/templates/basic.js
+++ demo/src/templates/basic.js
@@ -1,6 +1,6 @@
 import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
-import { css } from '@emotion/react';
+import styled from '@emotion/styled';
 import { graphql } from 'gatsby';
 import {
   ContributingGuidelines,
@@ -39,31 +39,8 @@ const BasicTemplate = ({ data, location, children }) => {
   return (
     <>
       <SEO location={location} title={frontmatter.title} />
-      <Layout.Main
-        css={css`
-          display: grid;
-          grid-template-areas:
-            'page-title page-tools'
-            'content page-tools';
-          grid-template-columns: minmax(0, 1fr) 320px;
-          grid-column-gap: var(--site-content-padding);
-
-          @media screen and (max-width: 760px) {
-            grid-template-areas:
-              'page-title'
-              'content'
-              'page-tools';
-            grid-template-columns: minmax(0, 1fr);
-          }
-        `}
-      >
-        <h1
-          css={css`
-            grid-area: page-title;
-          `}
-        >
-          {frontmatter.title}
-        </h1>
+      <Main>
+        <Title>{frontmatter.title}</Title>
         <Layout.Content>
           <MarkdownContainer>
             <MDX>{children}</MDX>
@@ -79,7 +56,7 @@ const BasicTemplate = ({ data, location, children }) => {
           <TableOfContents headings={headings} />
           <RelatedResources resources={relatedResources} />
         </Layout.PageTools>
-      </Layout.Main>
+      </Main>
     </>
   );
 };
@@ -89,6 +66,27 @@ BasicTemplate.propTypes = {
   data: PropTypes.object.isRequired,
   location: PropTypes.object.isRequired,
 };
+
+const Main = styled(Layout.Main)`
+  display: grid;
+  grid-template-areas:
+    'page-title page-tools'
+    'content page-tools';
+  grid-template-columns: minmax(0, 1fr) 320px;
+  grid-column-gap: var(--site-content-padding);
+
+  @media screen and (max-width: 760px) {
+    grid-template-areas:
+      'page-title'
+      'content'
+      'page-tools';
+    grid-template-columns: minmax(0, 1fr);
+  }
+`;
+
+const Title = styled.h1`
+  grid-area: page-title;
+`;
 
 export const pageQuery = graphql`
   query ($slug: String!) {
