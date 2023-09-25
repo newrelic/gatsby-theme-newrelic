0f42247e6efa554ba6af7ae92448b955ad3064f5
Sunny Zanchi <sunnyz@hey.com>
2023-09-25 13:01:29 -0400

    update jest and jest config for ESM
    using `babel-preset-gatsby` breaks the tests.
    `@babel/preset-react` is needed for JSX to work.
    not sure if `@babel/preset-env` is needed.
    adding the Gatsby mock to `jest-preprocess.mjs` was v important,
    basically no tests work without this

--- setup-test-env.mjs
+++ setup-test-env.mjs
@@ -10,3 +10,33 @@ global.ResizeObserver = class ResizeObserver {
   observe = jest.fn();
   disconnect = jest.fn();
 };
+
+jest.unstable_mockModule('gatsby', () => ({
+  __esModule: true,
+  graphql: () => {},
+  Link: ({ to, ...props }) => <a href={to} {...props} />,
+  useStaticQuery: () => ({
+    allLocale: {
+      nodes: [
+        {
+          name: 'English',
+          locale: 'en',
+          localizedPath: '/en',
+          isDefault: true,
+        },
+      ],
+    },
+    site: {
+      siteMetadata: {
+        siteUrl: 'https://github.com/foo/bar',
+        repository: 'https://foobar.net',
+      },
+    },
+    newRelicThemeConfig: {
+      tessen: {
+        product: 'foo',
+        subproduct: 'foobar',
+      },
+    },
+  }),
+}));
