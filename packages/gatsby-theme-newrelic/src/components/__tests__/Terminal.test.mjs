43623103fe981e0b61a0fd844831ded3b55e33f0
Sunny Zanchi <sunnyz@hey.com>
2023-10-05 11:20:00 -0400

    add doc comments & tests for Terminal

---
+++ packages/gatsby-theme-newrelic/src/components/__tests__/Terminal.test.mjs
@@ -0,0 +1,11 @@
+import React from 'react';
+import Terminal from '../Terminal';
+import { getByText, render } from '@testing-library/react';
+
+test('renders a terminal', () => {
+  const text = 'cd packages/gatsby-theme-newrelic';
+  const { container } = render(<Terminal>{text}</Terminal>);
+  const textNode = getByText(container, text);
+  expect(container.querySelector('code')).toBeInTheDocument();
+  expect(textNode).toBeTruthy();
+});
