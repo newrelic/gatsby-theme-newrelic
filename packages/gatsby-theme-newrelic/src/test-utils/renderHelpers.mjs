5d03be8aa79816649b798616ab3f7b9a96801b54
Liz Baker <lbaker@newrelic.com>
2023-10-09 16:17:12 -0700

    chore: Add QueryClientProvider for search test

--- packages/gatsby-theme-newrelic/src/test-utils/renderHelpers.mjs
+++ packages/gatsby-theme-newrelic/src/test-utils/renderHelpers.mjs
@@ -8,10 +8,12 @@ import {
   createMemorySource,
 } from '@reach/router';
 import LocaleProvider from '../components/LocaleProvider';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import translations from '../i18n/translations/en.json';
 import i18n from 'i18next';
 
 const { themeNamespace, i18nextOptions } = getI18nConfig({});
+const queryClient = new QueryClient();
 
 export const renderWithTranslation = (component, options) => {
   i18n.init({
@@ -51,7 +53,9 @@ export const renderWithProviders = (
   return render(
     <LocationProvider history={history}>
       <I18nextProvider i18n={i18n}>
-        <LocaleProvider i18n={i18n}>{component}</LocaleProvider>
+        <QueryClientProvider client={queryClient}>
+          <LocaleProvider i18n={i18n}>{component}</LocaleProvider>
+        </QueryClientProvider>
       </I18nextProvider>
     </LocationProvider>,
     options
