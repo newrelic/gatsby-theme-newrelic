db5598df9072b1805b52c514331f81bc770becf0
Sunny Zanchi <sunnyz@hey.com>
2023-12-06 14:03:43 -0500

    Revert "add test for `usePrevious`"
    This reverts commit 45269d23b4f1385007732b8f77496dc703560e2c.

--- packages/gatsby-theme-newrelic/src/hooks/usePrevious.js
+++ packages/gatsby-theme-newrelic/src/hooks/usePrevious.js
@@ -1,8 +1,5 @@
 import { useEffect, useRef } from 'react';
 
-/**
- * Keeps track of a value, always returning the second most recent value.
- */
 const usePrevious = (value, { initializeWithValue = false } = {}) => {
   const ref = useRef(initializeWithValue ? value : null);
 
