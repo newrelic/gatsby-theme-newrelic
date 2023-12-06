a1965b3db4c3f09bdb6de325ce1630126a5bde54
Sunny Zanchi <sunnyz@hey.com>
2023-12-06 14:01:29 -0500

    Revert "chore Add tests"
    This reverts commit 6e00e82f38401bef2676d2e6d750f839ac414769.

--- packages/gatsby-theme-newrelic/src/components/__tests__/CreateIssueButton.test.mjs
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import CreateIssueButton from '../CreateIssueButton.mjs';
-import { renderWithProviders } from '../../test-utils/renderHelpers.mjs';
-
-test('renders issue button', () => {
-  const { getByText } = renderWithProviders(<CreateIssueButton />);
-  expect(getByText(/Create issue/)).toBeInTheDocument();
-});
