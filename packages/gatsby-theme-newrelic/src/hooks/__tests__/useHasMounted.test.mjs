9c30eff7a4ae99503c0e29a1f661c8d67bd73ad8
Sunny Zanchi <sunnyz@hey.com>
2023-12-06 14:04:02 -0500

    Revert "feat: add useHasMounted test"
    This reverts commit 56f60009cedcdf07193f9889031ab6cbd5492aea.

--- packages/gatsby-theme-newrelic/src/hooks/__tests__/useHasMounted.test.mjs
+++
@@ -1,9 +0,0 @@
-import useHasMounted from '../useHasMounted';
-import { renderHook } from '@testing-library/react';
-
-test('sets hasMounted to true when rendered', () => {
-  const { result } = renderHook(() => useHasMounted());
-  console.log('result', result);
-
-  expect(result.current).toBe(true);
-});
