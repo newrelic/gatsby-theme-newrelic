af90e789cc7533629304b866b6c7309553c206d4
Liz Baker <lbaker@newrelic.com>
2024-01-02 13:03:22 -0800

    fix: Bring back merge deleted imports

--- packages/gatsby-theme-newrelic/src/components/Tabs/Pages.js
+++ packages/gatsby-theme-newrelic/src/components/Tabs/Pages.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { css } from '@emotion/react';
+import useTabs from './useTabs';
 
 const Pages = ({ children }) => {
   const { mobileBreakpoint } = useTabs();
