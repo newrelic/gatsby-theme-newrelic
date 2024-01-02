0b6fe85eb3d84ee3230a0d122f32ee810cf47625
Sunny Zanchi <sunnyz@hey.com>
2024-01-02 13:45:08 -0500

    add missing `border-top` on mobile

--- packages/gatsby-theme-newrelic/src/components/Tabs/Pages.js
+++ packages/gatsby-theme-newrelic/src/components/Tabs/Pages.js
@@ -5,7 +5,7 @@ import { css } from '@emotion/react';
 import useTabs from './useTabs';
 
 const Pages = ({ children }) => {
-  const { containerHeight, stacked, mobileBreakPoint } = useTabs();
+  const { containerHeight, stacked, mobileBreakpoint } = useTabs();
 
   return (
     <div
@@ -20,6 +20,10 @@ const Pages = ({ children }) => {
         overflow: hidden;
         position: relative;
 
+        @media screen and (max-width: ${mobileBreakpoint}) {
+          border-top: #afe2e3 solid 1px;
+        }
+
         ${stacked &&
         css`
           align-items: start;
@@ -27,7 +31,7 @@ const Pages = ({ children }) => {
           height: ${containerHeight}px;
           justify-content: center;
           width: 70%;
-          @media screen and (max-width: ${mobileBreakPoint}) {
+          @media screen and (max-width: ${mobileBreakpoint}) {
             width: 100%;
           }
         `}
