fe1acf714522e04bc9338c6a4bc0219f54214c56
Tabatha D Zeitke <47728020+tabathadelane@users.noreply.github.com>
2024-01-31 13:52:44 -0600

    fix: add custom attributes to browser agent events (#999)
    * fix: add custom attributes to browser agent events

--- packages/gatsby-theme-newrelic/gatsby/on-route-update.js
+++ packages/gatsby-theme-newrelic/gatsby/on-route-update.js
@@ -1,8 +1,18 @@
+import Cookies from 'js-cookie';
+
 import trackViaSegment from '../src/utils/page-tracking/segment';
 import getLocale from './utils/getLocale';
+import {
+  checkIfUserLoggedIn,
+  getSavedStatus as getSavedLoggedInStatus,
+} from '../src/hooks/useLoggedIn';
 
 const onRouteUpdate = ({ location, prevLocation }, themeOptions) => {
   trackViaSegment({ location, prevLocation }, themeOptions);
+  const getCookie = (key) => Cookies.get(key)?.replace(/%22/g, '') || null;
+  const loggedIn = getSavedLoggedInStatus() ?? checkIfUserLoggedIn();
+
+  const customer_user_id = getCookie('ajs_user_id');
 
   if (window.newrelic) {
     const locale = getLocale({ location }, themeOptions);
@@ -13,6 +23,13 @@ const onRouteUpdate = ({ location, prevLocation }, themeOptions) => {
     ) {
       window.newrelic.setCustomAttribute('locale', locale);
     }
+    // third param: persist = true - maintains the attribute value through the whole session
+    window.newrelic.setCustomAttribute('loggedIn', loggedIn, true);
+    window.newrelic.setCustomAttribute(
+      'customer_user_id',
+      customer_user_id,
+      true
+    );
   }
 };
 
