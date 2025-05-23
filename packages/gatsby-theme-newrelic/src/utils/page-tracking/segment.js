89888b8f213dc586e65435f4563bc8dfb952c882
gmanandhar-nr <gmanandhar@newrelic.com>
2025-05-23 13:14:35 +0530

    chore: Delay segment tracking by 30 seconds

--- packages/gatsby-theme-newrelic/src/utils/page-tracking/segment.js
+++ packages/gatsby-theme-newrelic/src/utils/page-tracking/segment.js
@@ -37,31 +37,76 @@ const trackPageView = ({ config, env, location, prevLocation, analytics }) => {
 
 const canSendPageView = (config) => config.section && config.platform;
 
+const getNRVisitedCookie = () => {
+  const cookieName = 'nr_pn_visited';
+  // Check if the cookie is already set
+  const existingCookie = Cookies.get(cookieName);
+
+  if(existingCookie) {
+    return existingCookie;
+  } else {
+    // If not set, set the cookie with the current Unix epoch time
+    const currentUnixEpoch = Math.floor(Date.now() / 1000); // Convert milliseconds to seconds
+    try {
+      Cookies.set(cookieName, currentUnixEpoch, { 
+        expires: 365, // Set cookie to expire in 1 year
+        domain: '.newrelic.com', // Set the domain to .newrelic.com
+        path: '/', // Set the path to /
+        sameSite: 'Lax' // Set the SameSite attribute to Lax
+      }); 
+    } catch (error) {
+      console.error('Failed to set cookie:', error);
+    }
+    return currentUnixEpoch;
+  }
+}
+
+
+
 const trackViaSegment = ({ location, prevLocation }, themeOptions) => {
   const env = getResolvedEnv(themeOptions);
   const segmentConfig = getSegmentConfig(themeOptions);
-  const analytics = AnalyticsBrowser.load({
-    writeKey:
-      env === 'production' || env === 'prod'
-        ? segmentConfig.segmentWriteKey
-        : DEV_SEGMENT_WRITE_KEY,
-  });
 
-  if (!segmentConfig) {
-    return;
-  }
-  // wrap inside a timeout to make sure react-helmet is done with its changes (https://github.com/gatsbyjs/gatsby/issues/11592)
-  requestAnimationFrame(() => {
+  const sendDataToSegment = () => {
+    const analytics = AnalyticsBrowser.load({
+      writeKey:
+        env === 'production' || env === 'prod'
+          ? segmentConfig.segmentWriteKey
+          : DEV_SEGMENT_WRITE_KEY,
+    });
+  
+    if (!segmentConfig) {
+      return;
+    }
+    // wrap inside a timeout to make sure react-helmet is done with its changes (https://github.com/gatsbyjs/gatsby/issues/11592)
     requestAnimationFrame(() => {
-      trackPageView({
-        config: segmentConfig,
-        env,
-        location,
-        prevLocation,
-        analytics,
+      requestAnimationFrame(() => {
+        trackPageView({
+          config: segmentConfig,
+          env,
+          location,
+          prevLocation,
+          analytics,
+        });
       });
     });
-  });
+  }
+
+  // get nr_pn_visited cookie. If it doesn't exist, set it
+  const visitedTimestamp = getNRVisitedCookie();
+
+  const delay = 30; // delay of 30 seconds before tracking cookies
+
+  if (visitedTimestamp && Math.round(Date.now() / 1000) - visitedTimestamp > delay) {
+    sendDataToSegment();
+  } else {
+    // Schedule sending data to Segment after the delay
+    const remainingDelay = delay - (Math.round(Date.now() / 1000) - visitedTimestamp);
+    setTimeout(() => {
+      sendDataToSegment();
+    }, remainingDelay * 1000); // Convert seconds to milliseconds
+  }
+
 };
 
 export default trackViaSegment;
